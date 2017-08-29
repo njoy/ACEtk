cce9a8e6ded50f2356e39acddc823586b17f88d1
David A. Dixon <ddixon@lanl.gov>
2017-08-29 17:28:09 -0600

    public methods and added rawOccupationNumbers in MP1.hpp

--- src/ACEtk/interpretation/EL03/AtomicLevels.hpp
+++ src/ACEtk/interpretation/EL03/AtomicLevels.hpp
@@ -1,6 +1,21 @@
 class AtomicLevels {
   const Table& table;
   
+  auto rawOccupationNumbers() const {
+    const auto length = this->table.data.NXS( 11 );
+    const auto start  = this->table.data.JXS( 11 );
+    return this->table.data.XSS( start, length );
+  }
+
+public:
+  AtomicLevels( const Table& table ) : table( table ) {}
+
+  auto occupationNumbers() const {
+    return this->rawOccupationNumbers()
+      | ranges::view::transform( []( const auto x )
+				 { return std::abs(x); } );
+  }  
+
   auto bindingEnergies() const {
     const auto length = this->table.data.NXS( 11 );
     const auto start  = this->table.data.JXS( 11 ) + length;
@@ -9,27 +24,16 @@ class AtomicLevels {
 				 { return entry * electronVolt; } );
   }
 
-  auto occupationNumbers() const {
-    const auto length = this->table.data.NXS( 11 );
-    const auto start  = this->table.data.JXS( 11 );
-    return this->table.data.XSS( start, length );
-  }
-
   auto isLevelConductive() const {
-    return this->occupationNumbers() |
-      ranges::view::transform( []( const auto entry ) { return entry < 0; } );
+    return this->occupationNumbers()
+      | ranges::view::transform( []( const auto entry )
+				 { return entry < 0; } );
   }  
   
-public:
-  AtomicLevels( const Table& table ) : table( table ) {}
-
   auto values() const {
-    auto absEntry = []( const auto entry ){ return std::abs(entry); };
-    return
-      ranges::view::zip( this->occupationNumbers()
-			 | ranges::view::transform( absEntry ), 
-			 this->isLevelConductive(),
-			 this->bindingEnergies() );
+    return ranges::view::zip( this->occupationNumbers()
+			      this->isLevelConductive(),
+			      this->bindingEnergies() );
   }
     
 };
