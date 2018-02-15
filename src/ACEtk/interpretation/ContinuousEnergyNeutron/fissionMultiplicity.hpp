121f8a8254d717bbac1e4256dde1dccd103ecb98
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-02-15 15:24:28 -0700

    Adding FissionMultiplicity variant.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/fissionMultiplicity.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/fissionMultiplicity.hpp
@@ -2,3 +2,6 @@ struct fissionMultiplicity {
   #include "ACEtk/interpretation/ContinuousEnergyNeutron/fissionMultiplicity/Polynomial.hpp"
   #include "ACEtk/interpretation/ContinuousEnergyNeutron/fissionMultiplicity/Tabulated.hpp"
 };
+
+using FissionMultiplicity = std::variant< fissionMultiplicity::Polynomial, 
+                                          fissionMultiplicity::Tabulated >;
