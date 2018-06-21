a7c176c98851b90e2002864832667070f84a22f6
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-06-21 14:57:24 -0600

    Finished with LAW4.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular.hpp
@@ -1,14 +1,19 @@
 class ContinuousTabular {
 
+public:
+#include "ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular/Distribution.hpp"
+
+private:
   ENDFInterpolationParameters interpolationParameters_;
   Table::Slice incidentEnergies_;
+  std::vector< Distribution > distributions_;
 
 public:
 #include "ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular/src/ctor.hpp"
-#include "ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular/Distribution.hpp"
 
   auto interpolationParameters() const { return interpolationParameters_; }
   auto incidentEnergies() const { return incidentEnergies_; }
+  auto distributions() const{ return distributions_; }
 
 };
 
--- src/ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular/test/ContinuousTabular.test.cpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/EnergyDistributionLaws/ContinuousTabular/test/ContinuousTabular.test.cpp
@@ -8,21 +8,57 @@ using namespace njoy::ACEtk::interpretation;
 SCENARIO( "Law4" ){
   std::vector< double > NBT{ 1, 3 };
   std::vector< double > INT{ 2, 1 };
-  std::vector< double > energies{ 1.0, 2.0, 3.0, 4.0, 5.0 };
+  std::vector< double > energies{ 1.0, 2.0, 3.0 };
+
+  int INTT{ 3 };
+  std::vector< double > dEnergies{ 10.0, 11.0, 12.0 };
+  std::vector< double > PDF{ 0.2, 0.5, 0.3 };
+  std::vector< double > CDF{ 0.2, 0.7, 1.0 };
+  
+  using Dist = ContinuousEnergyNeutron::ContinuousTabular::Distribution;
+  std::vector< Dist > dists{
+    Dist{ INTT, dEnergies, PDF, CDF },
+    Dist{ INTT, dEnergies, PDF, CDF },
+    Dist{ INTT, dEnergies, PDF, CDF }
+  };
+
   GIVEN( "valid input values" ){
     WHEN( "constructing a LAW4" ){
       ContinuousEnergyNeutron::ContinuousTabular law4( 
-        NBT, INT, energies );
+        NBT, INT, energies, std::move( dists ) );
 
       THEN( "the parameters can be verified" ){
         auto eip = law4.interpolationParameters();
         REQUIRE( ranges::equal( NBT, eip.NBT() ) );
         REQUIRE( ranges::equal( INT, eip.schemes() ) );
         REQUIRE( ranges::equal( energies, law4.incidentEnergies() ) );
+
+        REQUIRE( 3 == law4.distributions().size() );
       }
     }
   } // GIVEN valid
 
   GIVEN( "invalid input values" ){
+    WHEN( "wrong number of inerpolation regions" ){
+      std::vector< double > NBT{ 1, 2, 3, 4, 5 };
+      std::vector< double > INT{ 1, 2, 3, 4, 5 };
+      THEN( "an exception is thrown" ){
+        REQUIRE_THROWS( ContinuousEnergyNeutron::ContinuousTabular( 
+          NBT, INT, energies, std::move( dists ) ) );
+      }
+    }
+    WHEN( "wrong number of distributions" ){
+      std::vector< Dist > dists{
+        Dist{ INTT, dEnergies, PDF, CDF },
+        Dist{ INTT, dEnergies, PDF, CDF },
+        Dist{ INTT, dEnergies, PDF, CDF },
+        Dist{ INTT, dEnergies, PDF, CDF }
+      };
+
+      THEN( "an exception is thrown" ){
+        REQUIRE_THROWS( ContinuousEnergyNeutron::ContinuousTabular( 
+          NBT, INT, energies, std::move( dists ) ) );
+      }
+    }
   } // GIVEN invalid
 } // SCENARIO
