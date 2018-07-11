c0d55240c23eee841e87af86e1dd31c4eaf0dff5
Kyle Stuart Beling <kbeling@lanl.gov>
2018-07-11 08:31:58 -0600

    Added transpose, and added tests for values

--- src/ACEtk/interpretation/EL03/MottScatteringCorrection.hpp
+++ src/ACEtk/interpretation/EL03/MottScatteringCorrection.hpp
@@ -1,10 +1,10 @@
 class MottScatteringCorrection {
   const Table& table;
 
-  template<int N>
   auto h() const {
-    const auto length = this->table.data.NXS( 4 ) ;
-    const auto start  = this->table.data.JXS( 3 ) + N * length;
+    constexpr int num_angles = 5;
+    const auto length = num_angles * this->table.data.NXS( 4 );
+    const auto start  = this->table.data.JXS( 3 ) + this->table.data.NXS( 4 );
     return this->table.data.XSS( start, length ) | ranges::view::reverse;
   }
   
@@ -22,12 +22,22 @@ public:
   }
 
   auto values() const {
+
+    const auto num_energies = this->table.data.NXS( 4 );
     
-      return ranges::view::zip( this->h<1>(),
-				this->h<2>(),
-				this->h<3>(),
-				this->h<4>(),
-				this->h<5>() );
+    auto transpose = ranges::make_pipeable( [ num_energies ] ( auto&& ror ){ 
+	auto indices = ranges::view::iota( 0, num_energies );
+	auto get_entry = [](auto index){
+	  return [index](auto&& range)->decltype(auto){ return range[index]; };
+	};
+	
+	return indices | ranges::view::transform( [ror, get_entry ]( auto index ){
+	    return ror | ranges::view::transform( get_entry(index) ); } );
+      } );
+    
+    return this->h() | ranges::view::chunk( num_energies )
+                     | transpose
+                     | ranges::view::transform( ranges::view::reverse );
+    
   }
-  
 };
--- src/ACEtk/interpretation/EL03/test/MottScatteringCorrection.test.cpp
+++ src/ACEtk/interpretation/EL03/test/MottScatteringCorrection.test.cpp
@@ -11,7 +11,16 @@ namespace{
       1.000000000000E+00, 7.000000000000E-01, 4.000000000000E-01,
       2.000000000000E-01, 1.000000000000E-01, 7.000000000000E-02,
       4.000000000000E-02, 2.000000000000E-02, 1.000000000000E-02,
-      5.000000000000E-03, 1.000000000000E-03, 1.000000000000E-04 }};      
+      5.000000000000E-03, 1.000000000000E-03, 1.000000000000E-04 }};
+
+  std::array< double, 5 > referenceMottCorrections1 = {{
+	1.000000000000E+00, 8.589500000000E-01, 5.057200000000E-01,
+	1.472600000000E-01, 0.000000000000E+00 }};
+
+    std::array< double, 5 > referenceMottCorrections2 = {{
+	1.000000000000E+00, 1.000080000000E+00, 9.999400000000E-01,
+	9.998300000000E-01, 9.997200000000E-01 }};
+
 }
 
 SCENARIO("test interpretation::EL03::MottScatteringCorrection.hpp"){  
@@ -21,20 +30,25 @@ SCENARIO("test interpretation::EL03::MottScatteringCorrection.hpp"){
     const auto el03 = interpretation::EL03( table );
 
     WHEN("Querying for the energy grid in MeV,"
-	 "on which the mott scattering cross section data are evaluated") {
+	 "on which the mott scattering correction data are evaluated") {
       const auto trial = el03.mottScatteringCorrection().energyGrid();
       const auto reference = referenceEnergyGrid | ranges::view::reverse;
-      for( const auto pair : ranges::view::zip( reference, trial ) ) {
+      for ( const auto pair : ranges::view::zip( reference, trial ) ) {
 	REQUIRE( pair.first == Approx( pair.second.value ) );
       }
     }
     
-    WHEN("Querying for the mott scattering cross section data"){
-      const auto trial = el03.mottScatteringCorrection().values();
-      ranges::for_each( const auto group, trial ) {
-	std::cout << group.first << "\n";
+    WHEN("Querying for the Mott scattering correction data"){
+      const auto mottCorrectionValues = el03.mottScatteringCorrection().values();
+      for ( const auto pair
+	      : ranges::view::zip( referenceMottCorrections1, mottCorrectionValues[17] ) ) {
+	REQUIRE( pair.first == Approx( pair.second ) );
       }
-	
+      
+      for ( const auto pair
+	      : ranges::view::zip( referenceMottCorrections2, mottCorrectionValues[0] ) ) {
+	REQUIRE( pair.first == Approx( pair.second ) );
+      }      
     }
   }
 }
