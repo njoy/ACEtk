5fb3a1ed3e19f3985de03bb27f2c26de0e73f7a1
Kyle Stuart Beling <kbeling@lanl.gov>
2018-07-17 08:46:35 -0600

    Fixed constexpr num_params, fixed travis.yml

--- src/ACEtk/interpretation/EL03/RileyCrossSection.hpp
+++ src/ACEtk/interpretation/EL03/RileyCrossSection.hpp
@@ -1,17 +1,17 @@
 class RileyCrossSection {
   const Table& table;
   static constexpr int length = 126;
-  static constexpr int params_per_energy = 14;
-    
+
+  static constexpr int params_per_energy() { return 14; }
+  
 public:
   RileyCrossSection( const Table& table ) : table(table) {}
 
   auto energyGrid() const {
     const auto start  = this->table.data.JXS( 4 );
-    constexpr int num_params = 14;
     return
       this->table.data.XSS( start, RileyCrossSection::length )
-      | ranges::view::stride( num_params )
+      | ranges::view::stride( params_per_energy() )
       | ranges::view::reverse
       | ranges::view::transform ( [] ( auto && entry )
 				  { return entry * kilo(electronVolt); } );
@@ -19,11 +19,10 @@ public:
 
   auto values() const {
     const auto start = this->table.data.JXS( 4 );
-    constexpr int num_params = 14;    
     return
       this->table.data.XSS( start, RileyCrossSection::length )
       | ranges::view::reverse
-      | ranges::view::chunk( num_params )
+      | ranges::view::chunk( params_per_energy() )
       | ranges::view::transform( ranges::view::reverse )
       | ranges::view::transform( ranges::view::drop_exactly(1) );
   }
