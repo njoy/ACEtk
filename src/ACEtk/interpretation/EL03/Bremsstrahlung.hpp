9a1b26fccbe50cf2f2524362f80cfb6531cc5d31
Kyle Stuart Beling <kbeling@lanl.gov>
2018-07-17 13:59:56 -0600

    Cleaned up Bremsstrahlung test, and function renaming

--- src/ACEtk/interpretation/EL03/Bremsstrahlung.hpp
+++ src/ACEtk/interpretation/EL03/Bremsstrahlung.hpp
@@ -1,7 +1,6 @@
 class Bremsstrahlung {
   const Table& table;
 
-
 public:
   Bremsstrahlung( const Table& table ) : table( table ) {}
   
@@ -20,20 +19,24 @@ public:
   }
 
   auto values() const {
-    const auto length = this->table.data.NXS( 5 )*this->table.data.NXS( 6 );
+    const auto length = this->table.data.NXS( 5 ) * this->table.data.NXS( 6 );
     const auto start  = this->table.data.JXS( 5 ) + this->table.data.NXS( 5 )
                                                   + this->table.data.NXS( 6 );
+    const auto units = [](auto&& entry) { return entry * centi(meter)
+					  * centi(meter) / mega(electronVolt); };
+
     return this->table.data.XSS( start, length )
-           | ranges::view::chunk( this->table.data.NXS( 5 ) );
+      | ranges::view::transform( units )
+      | ranges::view::chunk( this->table.data.NXS( 5 ) );
   }
 
-  auto photonElectronRatioValuesSpectrumCalculation() const {
+  auto photonElectronRatiosForSpectra() const {
     const auto length = this->table.data.NXS( 9 );
     const auto start  = this->table.data.JXS( 9 );
     return this->table.data.XSS( start, length );
   }
 
-  auto photonElectronRatioValuesAngularDistribution() const {
+  auto photonElectronRatiosForAngle() const {
     const auto length = this->table.data.NXS( 10 );
     const auto start  = this->table.data.JXS( 10 );
     return this->table.data.XSS( start, length );
