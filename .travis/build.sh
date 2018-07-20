bffb6b257c1bc5eef155b447fac78387c54d99db
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-07-20 14:31:47 -0600

    Updating Travis configuration.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-set -x
 
-function repeat {
-    while true
-    do
-        sleep $1
-        ${@:2}
-    done
-}
-       
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives \
     --install /usr/bin/gcc gcc /usr/bin/gcc-6 90 \
@@ -20,71 +11,35 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo update-alternatives --config gcc
   sudo update-alternatives --config clang
   if [ "$CXX" = "clang++" ]; then
+    export appended_flags=$appended_flags"-stdlib=libstdc++"
     export PATH=/usr/bin:$PATH
-    export CUSTOM='-D link_time_optimization=OFF'
-  else
-    export CUSTOM='-D link_time_optimization=ON -D CMAKE_AR=/usr/bin/gcc-ar -D CMAKE_NM=/usr/bin/gcc-nm -D CMAKE_RANLIB=/usr/bin/gcc-ranlib'
+    export NOPE='-D no_link_time_optimization=TRUE'
   fi;
 fi
 
-if [ "$build_type" = "coverage" ]; then
-  export build_type=DEBUG
-  export coverage=true
-  export CUSTOM="$CUSTOM -D coverage=ON"
-else
-  export coverage=false
-fi;
-
+./fetch_subprojects.py
 mkdir build
 cd build
-       
-cmake -D CMAKE_BUILD_TYPE=$build_type \
+cmake -D build_type=$build_type \
       -D static_libraries=$static_libraries \
-      -D NJOY21_appended_flags="$appended_flags" \
-      $CUSTOM .. &> configuration.txt
-export CONFIGURATION_FAILURE=$?
+      -D appended_flags="$appended_flags" \
+      $NOPE ..
+make -j2
 
-if [ $CONFIGURATION_FAILURE -ne 0 ];
-then
-  echo "failed while configuring"
-  cat configuration.txt
-  exit 1
-fi
-
-repeat 300 echo "Still building..."&
-export EKG=$!
-       
-make VERBOSE=1 -j2 &> compilation.txt
 export COMPILATION_FAILURE=$?
-
-if [ $COMPILATION_FAILURE -ne 0 ];
-then
-  echo "failed while compiling"
-  cat compilation.txt  
-  exit 1
-fi
-
-kill $EKG
-
-ctest --output-on-failure -j2 &> testing.txt
+ctest --output-on-failure -j2
 export TEST_FAILURE=$?
-if [ $TEST_FAILURE -ne 0 ];
+
+if [ "$build_type" = "coverage" ]
 then
-    echo "failed while testing"
-    cat testing.txt  
+    pip install --user cpp-coveralls
+    echo "loading coverage information"
+    coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+fi
+
+if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
+then
     exit 1
+else
+    exit 0
 fi
-
-if $coverage; then
-  pip install --user cpp-coveralls &> coverage_upload.txt
-  echo "failed while loading coverage information"
-  coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp' >> coverage_upload.txt 2>&1
-  if [ $? -ne 0 ];
-  then
-     echo "failed while coverage report!"
-     cat coverage_upload.txt
-     exit 1
-  fi
-fi
-
-exit 0
