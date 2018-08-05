e06d176955b2104fdbd41d39c36988edc130244c
David A. Dixon <david8dixon@gmail.com>
2018-08-05 13:05:10 -0600

    added missing files

--- src/ACEtk/interpretation/EL03/Bremsstrahlung.hpp
+++ src/ACEtk/interpretation/EL03/Bremsstrahlung.hpp
@@ -4,13 +4,13 @@ class Bremsstrahlung {
 public:
   Bremsstrahlung( const Table& table ) : table( table ) {}
   
-  #include "ACEtk/interpretation/EL03/Bremsstrahlung/ProductionCrossSection.hpp"
+  #include "ACEtk/interpretation/EL03/Bremsstrahlung/src/ProductionCrossSection.hpp"
   
   auto productionCrossSection() const {
     return ProductionCrossSection{ this->table };
   }
 
-  #include "ACEtk/interpretation/EL03/Bremsstrahlung/OutgoingDistributions.hpp"  
+  #include "ACEtk/interpretation/EL03/Bremsstrahlung/src/OutgoingDistributions.hpp"  
 
   auto outgoingDistributions() const {
     return OutgoingDistributions{ this->table };
