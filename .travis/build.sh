be97cadc7b278b1cd2575e8030d16d9dda179fd8
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-10-31 15:58:50 -0600

    Fixing up the Travis build script.

--- .travis/build.sh
+++ .travis/build.sh
@@ -13,33 +13,44 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   if [ "$CXX" = "clang++" ]; then
     export appended_flags=$appended_flags"-stdlib=libstdc++"
     export PATH=/usr/bin:$PATH
-    export NOPE='-D no_link_time_optimization=TRUE'
+    export CUSTOM='-D no_link_time_optimization=TRUE'
   fi;
+  export NPROC=$(nproc)
+else
+  export NPROC=$(sysctl -n hw.physicalcpu)
 fi
 
-./fetch_subprojects.py
+if [ "$build_type" = "coverage" ]; then
+  export build_type=DEBUG
+  export coverage=true
+  export CUSTOM="$CUSTOM -D coverage=ON"
+else
+  export coverage=false
+fi;
+
 mkdir build
 cd build
-cmake -D build_type=$build_type \
+cmake -D CMAKE_BUILD_TYPE=$build_type \
       -D static_libraries=$static_libraries \
-      -D appended_flags="$appended_flags" \
-      $NOPE ..
-make -j2
-
+      -D ACEtk_appended_flags="$appended_flags" \
+      $CUSTOM ..
+make -j$NPROC
 export COMPILATION_FAILURE=$?
-ctest --output-on-failure -j2
-export TEST_FAILURE=$?
 
-if [ "$build_type" = "coverage" ]
+if [ $COMPILATION_FAILURE -ne 0 ];
 then
-    pip install --user cpp-coveralls
-    echo "loading coverage information"
-    coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
+  exit 1
 fi
 
-if [ $COMPILATION_FAILURE -ne 0 ] || [ $TEST_FAILURE -ne 0 ];
+ctest --output-on-failure -j$NPROC
+export TEST_FAILURE=$?
+if [ $TEST_FAILURE -ne 0 ];
 then
     exit 1
-else
-    exit 0
+fi
+
+if $coverage; then
+  pip install --user cpp-coveralls
+  echo "loading coverage information"
+  coveralls  --exclude-pattern "/usr/include/.*|.*/CMakeFiles/.*|.*subprojects.*|.*dependencies.*|.*test\.cpp" --root ".." --build-root "." --gcov-options '\-lp'
 fi
