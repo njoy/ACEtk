21910e318ee62299c57bb96a647ed85275126824
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-02-26 14:38:29 -0700

    Moving energy distribution tests to where they belong.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/Builder/test/Builder.test.cpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/Builder/test/Builder.test.cpp
@@ -6,6 +6,180 @@
 using namespace njoy::ACEtk;
 using namespace njoy::ACEtk::interpretation;
 
+
+template< typename B >
+void LAW1( B& ED ){
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0, 5.0, 6.0 };
+  std::vector< double > eout_v{ 1.1, 1.2, 1.3 };
+  std::array< double, 3 > eout_a{ 1.1, 1.2, 1.4 };
+
+  ED.tabularEquiprobableEnergyBins()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .outgoingEnergyTable( njoy::utility::copy( eout_v ) )
+      .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
+      .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
+      .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
+      .add(); // tabularEquiprobableEnergyBins
+}
+
+template< typename B >
+void LAW2( B& ED ){
+  ED.discretePhotonEnergy()
+      .energy( 3.14 )
+      .primaryFlag( 1 )
+      .add(); // discretePhotonEnergy
+}
+
+template< typename B >
+void LAW3( B& ED ){
+  ED.levelScattering()
+      .atomicWeightRatio( 235.98 )
+      .QValue( 2.765 )
+      .add(); // levelScattering
+}
+
+template< typename B >
+void LAW4( B& ED ){
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0 };
+
+  std::vector< int > INTT{ 1, 2 };
+  std::vector< double > ene{ 1.0, 2.0};
+  std::vector< double > pdf{ 0.25, 0.75 };
+  std::vector< double > cdf{ 0.25, 1.0 };
+
+  ED.continuousTabularDistribution()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .distributionData()
+        .interpolationParameter( INTT[ 0 ] )
+        .energies( njoy::utility::copy( ene ) )
+        .pdf( njoy::utility::copy( pdf ) )
+        .cdf( njoy::utility::copy( cdf ) )
+      .add() // distributionData
+      .distributionData()
+        .interpolationParameter( INTT[ 1 ] )
+        .energies( njoy::utility::copy( ene ) )
+        .pdf( njoy::utility::copy( pdf ) )
+        .cdf( njoy::utility::copy( cdf ) )
+      .add() // distributionData
+    .add(); // continuousTabularDistribution
+}
+
+template< typename B >
+void LAW5( B& ED ){
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0 };
+  std::vector< double > theta{ 2.5, 3.7 };
+  std::vector< double > X{ 1.0, 2.2, 5.4 };
+
+  ED.generalEvaporationSpectrum()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .effectiveTemperature( njoy::utility::copy( theta ) )
+      .equiprobableBins( njoy::utility::copy( X ) )
+    .add(); // generalEvaporationSpectrum
+}
+
+template< typename B >
+void LAW7( B& ED ){
+
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0 };
+  std::vector< double > theta{ 2.5, 3.7 };
+  double U{ 3.14 };
+
+  ED.simpleMaxwellianFissionSpectrum()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .effectiveTemperature( njoy::utility::copy( theta ) )
+      .restrictionEnergy( U )
+    .add(); // simpleMaxwellianFissionSpectrum
+
+}
+
+template< typename B >
+void LAW9( B& ED ){
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0 };
+  std::vector< double > theta{ 2.5, 3.7 };
+  double U{ 3.14 };
+
+  ED.evaporationSpectrum()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .effectiveTemperature( njoy::utility::copy( theta ) )
+      .restrictionEnergy( U )
+    .add(); // evaporationSpectrum
+}
+
+template< typename B >
+void LAW11( B& ED ){
+  std::vector< int > aBoundaries{ 0, 3 };
+  std::vector< int > bBoundaries{ 1, 4 };
+  std::vector< int > aSchemes{ 2, 1 };
+  std::vector< int > bSchemes{ 2, 3 };
+  std::vector< double > aEnergies{ 1.0, 2.0, 3.0 };
+  std::vector< double > bEnergies{ 1.1, 2.1, 3.1 };
+  std::vector< double > a{ 0.1, 0.2, 0.3 };
+  std::vector< double > b{ 1.1, 1.2, 1.3 };
+  double restrictionEnergy{ 2.68 };
+
+  ED.energyDependentWattSpectrum()
+      .aTabulated()
+          .boundaries( njoy::utility::copy( aBoundaries ) )
+          .schemes( njoy::utility::copy( aSchemes ) )
+          .energies( njoy::utility::copy( aEnergies ) )
+          .values( njoy::utility::copy( a ) )
+        .add()
+      .bTabulated()
+          .boundaries( njoy::utility::copy( bBoundaries ) )
+          .schemes( njoy::utility::copy( bSchemes ) )
+          .energies( njoy::utility::copy( bEnergies ) )
+          .values( njoy::utility::copy( b ) )
+        .add()
+        .restrictionEnergy( restrictionEnergy )
+    .add(); // energyDependentWattSpectrum
+}
+
+template< typename B >
+void LAW22( B& ED ){
+  std::vector< int > boundaries{ 0, 3 };
+  std::vector< int > schemes{ 2, 1 };
+  std::vector< double > energies{ 1.0, 2.0 };
+  std::vector< double > P{ 0.1, 0.2, 0.4 };
+  std::vector< double > T{ 1.1, 1.2, 1.4 };
+  std::vector< double > C{ 2.1, 2.2, 2.4 };
+
+  ED.tabularLinearFunctions()
+      .boundaries( njoy::utility::copy( boundaries ) )
+      .schemes( njoy::utility::copy( schemes ) )
+      .energies( njoy::utility::copy( energies ) )
+      .distributionData()
+         .P( njoy::utility::copy( P ) )
+         .T( njoy::utility::copy( T ) )
+         .C( njoy::utility::copy( C ) )
+      .add() // distributionData
+      .distributionData()
+         .P( njoy::utility::copy( P ) )
+         .T( njoy::utility::copy( T ) )
+         .C( njoy::utility::copy( C ) )
+      .add() // distributionData
+    .add(); // tabularLinearFunctions
+}
+
 SCENARIO( "Testing EnergyDistribtion::Builder" ){
   ContinuousEnergyNeutron::Builder CENBuilder;
   using ParentBuilder = decltype( CENBuilder.reaction( 14 ) );
@@ -31,30 +205,48 @@ SCENARIO( "Testing EnergyDistribtion::Builder" ){
         .energies( njoy::utility::copy( energies ) )
         .probabilities( njoy::utility::copy( probabilities ) );
 
-    THEN( "a law can be given" ){
-      std::vector< double > eout_v{ 1.1, 1.2, 1.3 };
-      std::array< double, 3 > eout_a{ 1.1, 1.2, 1.4 };
+    std::vector< int > LAWS{ 1, 2, 3, 4, 5, 7, 9, 11, 22, /*24, 44, 61, 66, 67 */ };
 
-      tb.tabularEquiprobableEnergyBins()
-          .boundaries( njoy::utility::copy( boundaries ) )
-          .schemes( njoy::utility::copy( schemes ) )
-          .energies( njoy::utility::copy( energies ) )
-          .outgoingEnergyTable( njoy::utility::copy( eout_v ) )
-          .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
-          .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
-          .outgoingEnergyTable( njoy::utility::copy( eout_a ) )
-          .add();
+    THEN( "a LAW can be given" ){
+      for( auto LAW : LAWS ){
+        njoy::Log::info( "Trying LAW={}", LAW );
 
-      auto ED = tb.construct();
+        switch( LAW ){
+          case 1:
+            LAW1( tb );
+            break;
+          case 2:
+            LAW2( tb );
+            break;
+          case 3:
+            LAW3( tb );
+            break;
+          case 4:
+            LAW4( tb );
+            break;
+          case 5:
+            LAW5( tb );
+            break;
+          // case 7:
+          //   LAW7( tb );
+          //   break;
+          // case 9:
+          //   LAW9( tb );
+          //   break;
+          case 11:
+            LAW11( tb );
+            break;
+          case 22:
+            LAW22( tb );
+            break;
+          default:
+            njoy::Log::info( "I don't know what to do with LAW={}", LAW );
+        }
 
-      THEN( "the values of the distribution can be checked" ){
-        auto parameters = ED.tab1.parameters;
-        CHECK( ranges::equal( boundaries, parameters->first ) );
-        CHECK( ranges::equal( schemes, parameters->second ) );
-        CHECK( ranges::equal( energies, ED.tab1.x ) );
-        CHECK( probabilities == ED.tab1.y );
-      }
-    }
+        CHECK_NOTHROW( tb.construct() );
+
+      } // for LAW
+    } // THEN a law can be given
   } // GIVEN
   GIVEN( "invalid inputs" ){
     WHEN( "energies are negative" ){
