a28604af004fd1e45a4f6493b6c895b6342e178f
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-03-12 15:25:22 -0600

    Fixing some issues found by Clang compiler.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/Reaction/AngularDistribution/Equiprobable/Builder/src/values.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/Reaction/AngularDistribution/Equiprobable/Builder/src/values.hpp
@@ -2,9 +2,8 @@ template< typename Range,
           utility::Require< true, utility::is_range, Range > = true >
 Builder& values( Range&& bins ){
   try{ 
-    this->cosineBins_ = std::move( 
-        details::verify::CosineBins< std::array< double, 33 > >( 
-            details::make_array< 33 > (bins ) ) );
+    this->cosineBins_ = details::verify::CosineBins< std::array< double, 33 > >( 
+            details::make_array< 33 > (bins ) );
     return *this;
   } catch( details::verify::exceptions::InvalidCosine& c ){
     Log::info( "Trouble encountered constructing cosine bins" );
--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/Reaction/AngularDistribution/Tabulated/Builder/src/cosineGrid.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/Reaction/AngularDistribution/Tabulated/Builder/src/cosineGrid.hpp
@@ -2,9 +2,8 @@ template< typename Range,
           utility::Require< true, utility::is_range, Range > = true >
 Builder& cosineGrid( Range&& grid ){
   try{
-    this->cosineGrid_ = std::move(
-      details::verify::CosineBins< std::vector< double > >( 
-          std::forward< Range >( grid ) ) );
+    this->cosineGrid_ = details::verify::CosineBins< std::vector< double > >( 
+        std::forward< Range >( grid ) );
     return *this;
   } catch( details::verify::exceptions::InvalidCosine& c ){
     Log::info( "Trouble encountered constructing cosine grid" );
