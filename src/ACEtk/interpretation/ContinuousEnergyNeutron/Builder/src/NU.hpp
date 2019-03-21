ed51d57cdc665c9c6690a09557987f637fc3b709
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-03-21 16:31:46 -0600

    Trying to make NUBAR visitors work.
    Not being successful.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/src/NU.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/src/NU.hpp
@@ -4,8 +4,29 @@ void NU(){
   auto foundTotal = (
       fissionMultiplicity_.find( "total" ) != fissionMultiplicity_.end() );
 
-  if( foundPrompt or foundTotal ){
-    this->jxs[ 1 ] = this->xss.size() + 1;
+  auto nuVisitor = [&](auto& nubar ) -> void
+  {
+    xss |= ranges::action::push_back( nubar.ACEify() );
+  };
+
+  auto size = this->xss.size() + 1;
+  this->jxs[ 1 ] = size;
+  if( foundPrompt ){
+
+    // std::visit( nuVisitor, foundPrompt );
+    auto promptSize = this->xss.size();
+
+    if( foundTotal ){
+      // std::visit( nuVisitor, foundTotal );
+      auto totalSize = this->xss.size() - promptSize;
+      this->xss.insert( this->xss.begin() + size, -1*totalSize );
+    }
+  }
+  else if( foundTotal ){
+    // std::visit( nuVisitor, foundTotal );
+  }
+  else{
+    this->jxs[ 1 ] = 0;
   }
 
   if( not this->delayedPrecursors_.empty() ){
