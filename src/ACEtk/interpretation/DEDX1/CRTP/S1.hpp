506e2afcf34c65a9c45e2f4140975e1cc14bab87
David A. Dixon <david8dixon@gmail.com>
2019-03-26 15:00:23 -0600

    more refactoring in S1

--- src/ACEtk/interpretation/DEDX1/CRTP/S1.hpp
+++ src/ACEtk/interpretation/DEDX1/CRTP/S1.hpp
@@ -2,6 +2,9 @@ template<typename SubRange, typename Unit, typename Projection>
 class S1 : public SubRange, protected Projection {
 
   std::size_t size_;
+
+  #include "ACEtk/interpretation/DEDX1/CRTP/S1/src/computeBounds.hpp"
+  #include "ACEtk/interpretation/DEDX1/CRTP/S1/src/lowerBound.hpp"  
   
 public:
   template<typename...Args>
@@ -11,36 +14,15 @@ public:
     size_(size_) {}  
 
   auto floor(Unit parameter) const {
-    auto range = static_cast<SubRange>(*this);    
-    const auto& projection = static_cast<const Projection&>(*this);
-
-    {
-      auto it = range.begin();
-      decltype(auto) front = it[0];
-      decltype(auto) back = it[this->size_ - 1];
-      const auto min = projection(front);
-      const auto max = projection(back);
-      const bool ob = outOfBounds(parameter, min, max);
-    }
-    
-    const auto it = ranges::lower_bound(range, parameter, std::less<void>{}, projection);
+    auto range = static_cast<SubRange>(*this);
+    const auto& projection = static_cast<const Projection&>(*this);    
+    auto it = this->lowerBound(range, parameter);
     const bool match = projection(*it) == parameter;
     return *(ranges::prev(it, not match));
-  }    
+  }
 
   auto ceil(Unit parameter) {
     auto range = static_cast<SubRange>(*this);    
-    const auto& projection = static_cast<const Projection&>(*this);
-    
-    {
-      auto it = range.begin();
-      decltype(auto) front = it[0];
-      decltype(auto) back = it[this->size_ - 1];
-      const auto min = projection(front);
-      const auto max = projection(back);
-      const bool ob = outOfBounds(parameter, min, max);      
-    }
-
-    return *(ranges::lower_bound(range, parameter, std::less<void>{}, projection));
+    return *(this->lowerBound(range, parameter));
   }
 };
---
+++ src/ACEtk/interpretation/DEDX1/CRTP/S1/src/lowerBound.hpp
@@ -0,0 +1,12 @@
+auto lower_bound(const SubRange& range,
+		 const Unit parameter) const {
+  const auto& projection = static_cast<const Projection&>(*this);
+  { 
+    const auto bounds = this->computeBounds( range, projection );
+    const bool ob =
+      outOfBounds( parameter, std::get<0>(bounds), std::get<1>(bounds) );
+  }
+  return
+    ranges::lower_bound( range, parameter, std::less<void>{}, projection );
+}
+  
