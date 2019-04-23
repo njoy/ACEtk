262f8fabff0bc3121cee45f2d67f43aea0c2402f
Jeremy Conlin <jlconlin@lanl.gov>
2019-04-23 10:32:31 -0600

    Fixing a small bug.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/LaboratoryAngleEnergy/AngularDistribution/EnergyDistribution/Builder/src/pdf.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/LaboratoryAngleEnergy/AngularDistribution/EnergyDistribution/Builder/src/pdf.hpp
@@ -1,7 +1,7 @@
 template< typename Range,
           utility::Require< true, utility::is_range, Range > = true >
 Builder& pdf( Range&& pdf ){
-  this->cosineGrid_ = std::make_optional< dvCB< std::vector< double > > >( 
+  this->pdf_ = std::make_optional< dvP< std::vector< double > > >( 
     std::move( pdf | ranges::to_vector ) );
   return *this;
 }
