391af9ef22618279a42fe699596e99848e1a5e2e
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-09-04 12:25:55 -0600

    Moving Slice definitions to Slice.hpp.

--- src/ACEtk/Table.hpp
+++ src/ACEtk/Table.hpp
@@ -47,21 +47,6 @@ public:
                         std::istreambuf_iterator< char >{} } ){}
   
   #include "ACEtk/Table/src/print.hpp"
-
-  using Slice = decltype( std::declval< const Data >().XSS( 1, 2 ) );
-  using Pizza = ranges::slice_view< Table::Slice >;
-
-  static Slice slice( const std::vector< double >& S ){ 
-    return { S.begin(), S.end() }; 
-  }
-
-  template< typename Range,
-            typename iterator = typename std::decay_t< Range >::iterator,
-            utility::Require< true, std::is_constructible, Slice, 
-                              iterator, iterator > = true
-          >
-  static Slice slice( Range&& S ){ 
-    return { S.begin(), S.end() }; 
-  }
+  #include "ACEtk/Table/src/Slice.hpp"
 
 };
