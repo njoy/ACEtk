94cb8b0bc1c8a1c136c3aae1291aafc8f9e895bb
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-10-15 12:42:42 -0600

    Responding to additional Pull Request comments.

--- src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/EnergyDependentWattSpectrum/Builder/src/construct.hpp
+++ src/ACEtk/interpretation/ContinuousEnergyNeutron/Builder/EnergyDistribution/EnergyDependentWattSpectrum/Builder/src/construct.hpp
@@ -1,19 +1,13 @@
 EnergyDependentWattSpectrum construct(){
-  if( not aTabulated_ ){
-    Log::info( "aTabulated not built" );
-  }
-  if( not bTabulated_ ){
-    Log::info( "bTabulated not built" );
-  }
   try{
     return EnergyDependentWattSpectrum{ 
       std::move( aTabulated_.value() ),
       std::move( bTabulated_.value() ),
       restrictionEnergy_.value()
-
     };
   } catch( std::bad_optional_access& b ){
-    Log::error( "Part of Energy Dependent Watt Spectrum not yet built." );
+    Log::error( 
+      "Some component of Energy Dependent Watt Spectrum not yet built." );
     throw;
   }
 }
