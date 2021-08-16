397d088c2421cdc6d2ea245265730006b1ef7bc8
David A. Dixon <ddixon@lanl.gov>
2021-08-16 14:43:33 -0600

    data is a function not a member

--- src/ACEtk/interpretation/EL03/Bremsstrahlung/src/OutgoingDistributions.hpp
+++ src/ACEtk/interpretation/EL03/Bremsstrahlung/src/OutgoingDistributions.hpp
@@ -5,15 +5,15 @@ public:
   OutgoingDistributions( const Table& table ) : table( table ) {}
   
   auto photonElectronRatiosForEnergy() const {
-    const auto length = this->table.data.NXS( 9 );
-    const auto start  = this->table.data.JXS( 9 );
-    return this->table.data.XSS( start, length );
+    const auto length = this->table.data().NXS( 9 );
+    const auto start  = this->table.data().JXS( 9 );
+    return this->table.data().XSS( start, length );
   }
 
   auto photonElectronRatiosForAngle() const {
-    const auto length = this->table.data.NXS( 10 );
-    const auto start  = this->table.data.JXS( 10 );
-    return this->table.data.XSS( start, length );
+    const auto length = this->table.data().NXS( 10 );
+    const auto start  = this->table.data().JXS( 10 );
+    return this->table.data().XSS( start, length );
   }  
 };
 
--- src/ACEtk/interpretation/EL03/Bremsstrahlung/src/ProductionCrossSection.hpp
+++ src/ACEtk/interpretation/EL03/Bremsstrahlung/src/ProductionCrossSection.hpp
@@ -5,29 +5,29 @@ public:
   ProductionCrossSection( const Table& table ) : table( table ) {}
   
   auto electron_energies() const {
-    const auto length = this->table.data.NXS( 5 );
-    const auto start  = this->table.data.JXS( 5 );
-    return this->table.data.XSS( start, length )
+    const auto length = this->table.data().NXS( 5 );
+    const auto start  = this->table.data().JXS( 5 );
+    return this->table.data().XSS( start, length )
       | ranges::view::transform( [](auto&& entry )
 				 { return entry * mega(electronVolt); } );
   }
 
   auto photonElectronEnergyRatio() const {
-    const auto length = this->table.data.NXS( 6 );
-    const auto start  = this->table.data.JXS( 5 ) + this->table.data.NXS( 5 );
-    return this->table.data.XSS( start, length );
+    const auto length = this->table.data().NXS( 6 );
+    const auto start  = this->table.data().JXS( 5 ) + this->table.data().NXS( 5 );
+    return this->table.data().XSS( start, length );
   }
 
   auto values() const {
-    const auto length = this->table.data.NXS( 5 ) * this->table.data.NXS( 6 );
-    const auto start  = this->table.data.JXS( 5 ) + this->table.data.NXS( 5 )
-                                                  + this->table.data.NXS( 6 );
+    const auto length = this->table.data().NXS( 5 ) * this->table.data().NXS( 6 );
+    const auto start  = this->table.data().JXS( 5 ) + this->table.data().NXS( 5 )
+                                                  + this->table.data().NXS( 6 );
 
     const auto units = [](auto&& entry) { return entry * centi(meter)
 					  * centi(meter) / mega(electronVolt); };
 
-    return this->table.data.XSS( start, length )
+    return this->table.data().XSS( start, length )
       | ranges::view::transform( units )
-      | ranges::view::chunk( this->table.data.NXS( 5 ) );
+      | ranges::view::chunk( this->table.data().NXS( 5 ) );
   }
 };
