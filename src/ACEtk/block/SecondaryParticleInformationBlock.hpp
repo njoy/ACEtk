14416f15ae33ae29fc29eb6fb8f21f8d70ab33ca
Wim Haeck <whaeck@gmail.com>
2022-01-07 03:14:24 -0700

    Adding the NTRO block

---
+++ src/ACEtk/block/SecondaryParticleInformationBlock.hpp
@@ -0,0 +1,92 @@
+#ifndef NJOY_ACETK_BLOCK_SECONDARYPARTICLEINFORMATIONBLOCK
+#define NJOY_ACETK_BLOCK_SECONDARYPARTICLEINFORMATIONBLOCK
+
+// system includes
+
+// other includes
+#include "ACEtk/block/details/Base.hpp"
+
+namespace njoy {
+namespace ACEtk {
+namespace block {
+
+/**
+ *  @class
+ *  @brief The continuous energy NTRO block with the number of reactions for
+ *         each secondary particle type
+ *
+ *  The SecondaryParticleInformationBlock class contains the number of reactions
+ *  that produce each secondary particle type as given on the PTYPE.
+ *
+ *  The number of available particle types is stored in NXS(7).
+ */
+class SecondaryParticleInformationBlock : protected details::Base {
+
+  /* fields */
+  unsigned int ntype_;
+
+  /* auxiliary functions */
+  #include "ACEtk/block/SecondaryParticleInformationBlock/src/generateXSS.hpp"
+  #include "ACEtk/block/SecondaryParticleInformationBlock/src/verifySize.hpp"
+
+public:
+
+  /* constructor */
+  #include "ACEtk/block/SecondaryParticleInformationBlock/src/ctor.hpp"
+
+  /* methods */
+
+  /**
+   *  @brief Return the number of secondary particle types (excluding the
+   *         projectile)
+   */
+  unsigned int NTYPE() const { return this->ntype_; }
+
+  /**
+   *  @brief Return the number of secondary particle types (excluding the
+   *         projectile)
+   */
+  unsigned int numberAdditionalSecondaryParticleTypes() const {
+
+    return this->NTYPE();
+  }
+
+  /**
+   *  @brief Return the number of reactions for a secondary particle index
+   *
+   *  @param[in] index     the index (one-based)
+   */
+  auto NP( std::size_t index ) const { return this->XSS( index ); }
+
+  /**
+   *  @brief Return the number of reactions for a secondary particle index
+   *
+   *  @param[in] index     the index (one-based)
+   */
+  auto numberReactions( std::size_t index ) const { return this->NP( index ); }
+
+  /**
+   *  @brief Return the number of reactions for all secondary particle types
+   */
+  auto NPs() const { return this->XSS( 1, this->NTYPE() ); }
+
+  /**
+   *  @brief Return the number of reactions for all secondary particle types
+   */
+  auto numberReactionsForAllTypes() const { return this->NPs(); }
+
+  using Base::empty;
+  using Base::name;
+  using Base::length;
+  using Base::XSS;
+  using Base::begin;
+  using Base::end;
+};
+
+using NTRO = SecondaryParticleInformationBlock;
+
+} // block namespace
+} // ACEtk namespace
+} // njoy namespace
+
+#endif
---
+++ src/ACEtk/block/SecondaryParticleInformationBlock/src/ctor.hpp
@@ -0,0 +1,40 @@
+private:
+
+/**
+ *  @brief Private constructor
+ */
+SecondaryParticleInformationBlock( std::vector< unsigned int >&& numbers,
+                                   std::size_t ntype ) :
+  Base( "NTRO", generateXSS( std::move( numbers ) ) ),
+  ntype_( ntype ) {}
+
+public:
+
+SecondaryParticleInformationBlock() = default;
+
+SecondaryParticleInformationBlock( const SecondaryParticleInformationBlock& ) = default;
+SecondaryParticleInformationBlock( SecondaryParticleInformationBlock&& ) = default;
+
+/**
+ *  @brief Constructor
+ *
+ *  @param[in] numbers    the number of reactions for each secondary particle type
+ */
+SecondaryParticleInformationBlock( std::vector< unsigned int > numbers ) :
+  SecondaryParticleInformationBlock( std::move( numbers ), numbers.size() ) {}
+
+/**
+ *  @brief Constructor
+ *
+ *  @param[in] ntro    the begin iterator of the NTRO block in the XSS array
+ *  @param[in] end     the end iterator of the NTRO block in the XSS array
+ *  @param[in] ntype   the number of secondary particle types
+ */
+SecondaryParticleInformationBlock( Iterator ntro, Iterator end, unsigned int ntype ) :
+  Base( "NTRO", ntro, end ), ntype_( ntype ) {
+
+  verifySize( this->begin(), this->end(), this->NTYPE() );
+}
+
+SecondaryParticleInformationBlock& operator=( const SecondaryParticleInformationBlock& ) = default;
+SecondaryParticleInformationBlock& operator=( SecondaryParticleInformationBlock&& ) = default;
