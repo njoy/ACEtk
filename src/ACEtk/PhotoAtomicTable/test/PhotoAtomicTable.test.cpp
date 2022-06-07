e75c7b9c3c4d2e50bef9c8741596952244025f42
Wim Haeck <whaeck@gmail.com>
2022-06-07 14:26:13 -0600

    Testing a PhotoAtomicTable with an SWD block

--- src/ACEtk/PhotoAtomicTable/test/PhotoAtomicTable.test.cpp
+++ src/ACEtk/PhotoAtomicTable/test/PhotoAtomicTable.test.cpp
@@ -9,7 +9,8 @@
 // convenience typedefs
 using namespace njoy::ACEtk;
 
-void verifyChunk( const PhotoAtomicTable& );
+void verifyChunkMcplib( const PhotoAtomicTable& );
+void verifyChunkMcplib03( const PhotoAtomicTable& );
 
 SCENARIO( "PhotoAtomicTable" ){
 
@@ -29,7 +30,7 @@ SCENARIO( "PhotoAtomicTable" ){
       THEN( "a PhotoAtomicTable can be constructed and members can be "
             "tested" ) {
 
-        verifyChunk( chunk );
+        verifyChunkMcplib( chunk );
       }
 
       THEN( "the IZ array is correct" ) {
@@ -97,7 +98,7 @@ SCENARIO( "PhotoAtomicTable" ){
       THEN( "a PhotoAtomicTable can be constructed and members can be "
             "tested" ) {
 
-        verifyChunk( chunk );
+        verifyChunkMcplib( chunk );
       }
 
       THEN( "the IZ array is correct" ) {
@@ -151,9 +152,148 @@ SCENARIO( "PhotoAtomicTable" ){
       } // THEN
     } // WHEN
   } // GIVEN
+
+  GIVEN( "valid data for a PhotoAtomicTable - mcplib03" ) {
+
+    auto table = fromFile( "3000.03p" );
+    std::array< int32_t, 16 > iz = table.data().IZ();
+    std::array< double, 16 > aw = table.data().AW();
+    std::array< int64_t, 16 > nxs = table.data().NXS();
+    std::array< int64_t, 32 > jxs = table.data().JXS();
+    std::vector< double > xss = table.data().XSS();
+
+    WHEN( "constructing a PhotoAtomicTable from a table" ) {
+
+      PhotoAtomicTable chunk( std::move( table ) );
+
+      THEN( "a PhotoAtomicTable can be constructed and members can be "
+            "tested" ) {
+
+        verifyChunkMcplib03( chunk );
+      }
+
+      THEN( "the IZ array is correct" ) {
+
+        decltype(auto) iz_chunk = chunk.data().IZ();
+        CHECK( iz.size() == iz_chunk.size() );
+        for ( unsigned int i = 0; i < iz_chunk.size(); ++i ) {
+
+          CHECK( iz[i] == Approx( iz_chunk[i] ) );
+        }
+      } // THEN
+
+      THEN( "the AW array is correct" ) {
+
+        decltype(auto) aw_chunk = chunk.data().AW();
+        CHECK( aw.size() == aw_chunk.size() );
+        for ( unsigned int i = 0; i < aw_chunk.size(); ++i ) {
+
+          CHECK( aw[i] == Approx( aw_chunk[i] ) );
+        }
+      } // THEN
+
+      THEN( "the NXS array is correct" ) {
+
+        decltype(auto) nxs_chunk = chunk.data().NXS();
+        CHECK( nxs.size() == nxs_chunk.size() );
+        for ( unsigned int i = 0; i < nxs_chunk.size(); ++i ) {
+
+          CHECK( nxs[i] == Approx( nxs_chunk[i] ) );
+        }
+      } // THEN
+
+      THEN( "the JXS array is correct" ) {
+
+        decltype(auto) jxs_chunk = chunk.data().JXS();
+        CHECK( jxs.size() == jxs_chunk.size() );
+        for ( unsigned int i = 0; i < jxs_chunk.size(); ++i ) {
+
+          CHECK( jxs[i] == Approx( jxs_chunk[i] ) );
+        }
+      } // THEN
+
+      THEN( "the XSS array is correct" ) {
+
+        decltype(auto) xss_chunk = chunk.data().XSS();
+        CHECK( xss.size() == xss_chunk.size() );
+        for ( unsigned int i = 0; i < xss_chunk.size(); ++i ) {
+
+          CHECK( xss[i] == Approx( xss_chunk[i] ) );
+        }
+      } // THEN
+    } // WHEN
+
+    WHEN( "constructing a PhotoAtomicTable from its components" ) {
+
+      PhotoAtomicTable base( std::move( table ) );
+
+      PhotoAtomicTable chunk( base.header(),
+                              {  },
+                              {  },
+                              base.ESZG(), base.JINC(),
+                              base.JCOH(), base.LHNM(),
+                              base.SWD() );
+
+      THEN( "a PhotoAtomicTable can be constructed and members can be "
+            "tested" ) {
+
+        verifyChunkMcplib03( chunk );
+      }
+
+//      THEN( "the IZ array is correct" ) {
+//
+//        decltype(auto) iz_chunk = chunk.data().IZ();
+//        CHECK( iz.size() == iz_chunk.size() );
+//        for ( unsigned int i = 0; i < iz_chunk.size(); ++i ) {
+//
+//          CHECK( iz[i] == Approx( iz_chunk[i] ) );
+//        }
+//      } // THEN
+//
+//      THEN( "the AW array is correct" ) {
+//
+//        decltype(auto) aw_chunk = chunk.data().AW();
+//        CHECK( aw.size() == aw_chunk.size() );
+//        for ( unsigned int i = 0; i < aw_chunk.size(); ++i ) {
+//
+//          CHECK( aw[i] == Approx( aw_chunk[i] ) );
+//        }
+//      } // THEN
+//
+//      THEN( "the NXS array is correct" ) {
+//
+//        decltype(auto) nxs_chunk = chunk.data().NXS();
+//        CHECK( nxs.size() == nxs_chunk.size() );
+//        for ( unsigned int i = 0; i < nxs_chunk.size(); ++i ) {
+//
+//          CHECK( nxs[i] == Approx( nxs_chunk[i] ) );
+//        }
+//      } // THEN
+//
+//      THEN( "the JXS array is correct" ) {
+//
+//        decltype(auto) jxs_chunk = chunk.data().JXS();
+//        CHECK( jxs.size() == jxs_chunk.size() );
+//        for ( unsigned int i = 0; i < jxs_chunk.size(); ++i ) {
+//
+//          CHECK( jxs[i] == Approx( jxs_chunk[i] ) );
+//        }
+//      } // THEN
+//
+//      THEN( "the XSS array is correct" ) {
+//
+//        decltype(auto) xss_chunk = chunk.data().XSS();
+//        CHECK( xss.size() == xss_chunk.size() );
+//        for ( unsigned int i = 0; i < xss_chunk.size(); ++i ) {
+//
+//          CHECK( xss[i] == Approx( xss_chunk[i] ) );
+//        }
+//      } // THEN
+    } // WHEN
+  } // GIVEN
 } // SCENARIO
 
-void verifyChunk( const PhotoAtomicTable& chunk ) {
+void verifyChunkMcplib( const PhotoAtomicTable& chunk ) {
 
   CHECK( "1000.01p" == chunk.ZAID() );
   CHECK( 0. == Approx( chunk.temperature() ) );
@@ -229,4 +369,113 @@ void verifyChunk( const PhotoAtomicTable& chunk ) {
 
   // SWD block
   CHECK( true == chunk.SWD().empty() );
+}
+
+void verifyChunkMcplib03( const PhotoAtomicTable& chunk ) {
+
+  CHECK( "3000.03p" == chunk.ZAID() );
+  CHECK( 0. == Approx( chunk.temperature() ) );
+
+//  CHECK( 821 == chunk.length() );
+  CHECK( 82 == chunk.NES() );
+  CHECK( 82 == chunk.numberEnergyPoints() );
+
+  // ESZG block
+  CHECK( false == chunk.ESZG().empty() );
+
+  CHECK( 82 == chunk.ESZG().NES() );
+  CHECK( 82 == chunk.ESZG().numberEnergyPoints() );
+
+  CHECK( 82 == chunk.ESZG().energies().size() );
+  CHECK( 82 == chunk.ESZG().incoherent().size() );
+  CHECK( 82 == chunk.ESZG().coherent().size() );
+  CHECK( 82 == chunk.ESZG().photoelectric().size() );
+  CHECK( 82 == chunk.ESZG().pairproduction().size() );
+
+  CHECK( -6.907755278982E+00 == Approx( chunk.ESZG().energies().front() ) );
+  CHECK(  1.151292546497E+01 == Approx( chunk.ESZG().energies().back() ) );
+  CHECK( -1.036037569528E+00 == Approx( chunk.ESZG().incoherent().front() ) );
+  CHECK( -9.880667912376E+00 == Approx( chunk.ESZG().incoherent().back() ) );
+  CHECK(  1.556749961755E+00 == Approx( chunk.ESZG().coherent().front() ) );
+  CHECK( -3.192069466597E+01 == Approx( chunk.ESZG().coherent().back() ) );
+  CHECK(  7.753623546560E+00 == Approx( chunk.ESZG().photoelectric().front() ) );
+  CHECK( -2.663665884863E+01 == Approx( chunk.ESZG().photoelectric().back() ) );
+  CHECK(                   0 == Approx( chunk.ESZG().pairproduction().front() ) );
+  CHECK( -2.222019445154E+00 == Approx( chunk.ESZG().pairproduction().back() ) );
+
+  // JINC block
+  CHECK( false == chunk.JINC().empty() );
+
+  CHECK( 21 == chunk.JINC().NM() );
+  CHECK( 21 == chunk.JINC().numberValues() );
+
+  CHECK( 21 == chunk.JINC().momentum().size() );
+  CHECK( 0.   == Approx( chunk.JINC().momentum().front() ) );
+  CHECK( 8.   == Approx( chunk.JINC().momentum().back() ) );
+
+  CHECK( 21 == chunk.JINC().values().size() );
+  CHECK( 0 == Approx( chunk.JINC().values().front() ) );
+  CHECK( 3 == Approx( chunk.JINC().values().back() ) );
+
+  // JCOH block
+  CHECK( false == chunk.JCOH().empty() );
+
+  CHECK( 55 == chunk.JCOH().NM() );
+  CHECK( 55 == chunk.JCOH().numberValues() );
+
+  CHECK( 55 == chunk.JCOH().momentum().size() );
+  CHECK( 0.   == Approx( chunk.JCOH().momentum().front() ) );
+  CHECK( 6.   == Approx( chunk.JCOH().momentum().back() ) );
+
+  CHECK( 55 == chunk.JCOH().integratedFormFactors().size() );
+  CHECK( 0. == Approx( chunk.JCOH().integratedFormFactors().front() ) );
+  CHECK( 9.972763100947E-02 == Approx( chunk.JCOH().integratedFormFactors().back() ) );
+
+  CHECK( 55 == chunk.JCOH().formFactors().size() );
+  CHECK( 3. == Approx( chunk.JCOH().formFactors().front() ) );
+  CHECK( 7.806310000000E-04 == Approx( chunk.JCOH().formFactors().back() ) );
+
+  // LHNM block
+  CHECK( false == chunk.LHNM().empty() );
+
+  CHECK( 82 == chunk.LHNM().NES() );
+  CHECK( 82 == chunk.LHNM().numberEnergyPoints() );
+  CHECK( 82 == chunk.LHNM().heating().size() );
+
+  CHECK( 9.978170710722E-04 == Approx( chunk.LHNM().heating().front() ) );
+  CHECK( 9.999428234605E+04 == Approx( chunk.LHNM().heating().back() ) );
+
+  // SWD block
+  CHECK( false == chunk.SWD().empty() );
+
+  CHECK( 2 == chunk.SWD().NSH() );
+
+  CHECK( 1 == chunk.SWD().LSWD( 1 ) );
+  CHECK( 96 == chunk.SWD().LSWD( 2 ) );
+
+  auto profile = chunk.SWD().comptonProfile(1);
+  CHECK( 2 == profile.interpolation() );
+  CHECK( 31 == profile.numberValues() );
+  CHECK( 31 == profile.momentum().size() );
+  CHECK( 0. == Approx( profile.momentum().front() ) );
+  CHECK( 100. == Approx( profile.momentum().back() ) );
+  CHECK( 31 == profile.pdf().size() );
+  CHECK( 6.527863344596e-01 == Approx( profile.pdf().front() ) );
+  CHECK( 3.571475386101e-10 == Approx( profile.pdf().back() ) );
+  CHECK( 31 == profile.cdf().size() );
+  CHECK( 0. == Approx( profile.cdf().front() ) );
+  CHECK( 1. == Approx( profile.cdf().back() ) );
+
+  profile = chunk.SWD().comptonProfile(2);
+  CHECK( 2 == profile.interpolation() );
+  CHECK( 31 == profile.numberValues() );
+  CHECK( 31 == profile.momentum().size() );
+  CHECK( 0. == Approx( profile.momentum().front() ) );
+  CHECK( 100. == Approx( profile.momentum().back() ) );
+  CHECK( 31 == profile.pdf().size() );
+  CHECK( 3.848272047027e+00 == Approx( profile.pdf().front() ) );
+  CHECK( 8.728039694288e-12 == Approx( profile.pdf().back() ) );
+  CHECK( 31 == profile.cdf().size() );
+  CHECK( 0. == Approx( profile.cdf().front() ) );
+  CHECK( 1. == Approx( profile.cdf().back() ) );
 }
