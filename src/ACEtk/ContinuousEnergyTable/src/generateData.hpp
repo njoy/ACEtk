6fc8df88091ab32cedb62146dce50c846f485cf2
Wim Haeck <whaeck@gmail.com>
2023-04-02 12:22:03 -0600

    Putting move back

--- src/ACEtk/ContinuousEnergyTable/src/generateData.hpp
+++ src/ACEtk/ContinuousEnergyTable/src/generateData.hpp
@@ -332,5 +332,6 @@ Data generateData( unsigned int z, unsigned int a,
   nxs[9] = z;
   nxs[10] = a;
 
-  return { iz, aw, std::move( nxs ), std::move( jxs ), std::move( xss ) };
+  return { std::move( iz ), std::move( aw ),
+           std::move( nxs ), std::move( jxs ), std::move( xss ) };
 }
