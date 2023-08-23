878287640591697e5d98409eb78e247fa300b89b
Wim Haeck <whaeck@gmail.com>
2023-08-23 15:06:53 -0600

    Adding the eprdata subshell block

---
+++ src/ACEtk/block/PhotoatomicElectronSubshellBlock.hpp
@@ -0,0 +1,162 @@
+#ifndef NJOY_ACETK_BLOCK_PHOTOATOMICELECTRONSUBSHELLBLOCK
+#define NJOY_ACETK_BLOCK_PHOTOATOMICELECTRONSUBSHELLBLOCK
+
+// system includes
+
+// other includes
+#include "ACEtk/block/details/ArrayData.hpp"
+
+namespace njoy {
+namespace ACEtk {
+namespace block {
+
+/**
+ *  @class
+ *  @brief The photoatomic SUBSH block with the electron subshell data
+ *
+ *  The PhotoatomicElectronSubshellBlock class contains 5 arrays of the
+ *  same length:
+ *    - the ENDF designator of each subshell
+ *    - the electron population for each subshell
+ *    - the binding energy for each subshell
+ *    - the vacancy cumulative probability for each subshell
+ *    - the number of possible transitions to fill a vacancy for each subshell
+ *
+ *  The size of each (the total number of electron shells) is stored in NXS(7).
+ */
+class PhotoatomicElectronSubshellBlock : protected details::ArrayData {
+
+  /* fields */
+
+  /* auxiliary functions */
+
+public:
+
+  /* constructor */
+  #include "ACEtk/block/PhotoatomicElectronSubshellBlock/src/ctor.hpp"
+
+  /**
+   *  @brief Return the number of electron subshells
+   */
+  unsigned int NSSH() const { return this->N(); }
+
+  /**
+   *  @brief Return the number of electron shells
+   */
+  unsigned int numberElectronSubshells() const { return this->NSSH(); }
+
+  /**
+   *  @brief Return the ENDF designator for each subshell
+   */
+  auto ENDF() const { return this->darray( 1 ); }
+
+  /**
+   *  @brief Return the ENDF designator for each subshell
+   */
+  auto designators() const { return this->ENDF(); }
+
+  /**
+   *  @brief Return the electron population for each subshell
+   */
+  auto EP() const { return this->darray( 2 ); }
+
+  /**
+   *  @brief Return the electron population for each subshell
+   */
+  auto populations() const { return this->EP(); }
+
+  /**
+   *  @brief Return the binding energies for each subshell
+   */
+  auto BE() const { return this->darray( 3 ); }
+
+  /**
+   *  @brief Return the binding energies for each subshell
+   */
+  auto bindingEnergies() const { return this->BE(); }
+
+  /**
+   *  @brief Return the vacancy probabilities for each subshell
+   */
+  auto CV() const { return this->darray( 4 ); }
+
+  /**
+   *  @brief Return the vacancy probabilities for each subshell
+   */
+  auto vacancyProbabilyties() const { return this->CV(); }
+
+  /**
+   *  @brief Return the number of transitions to fill a vacancy for each subshell
+   */
+  auto NT() const { return this->darray( 4 ); }
+
+  /**
+   *  @brief Return the number of transitions to fill a vacancy for each subshell
+   */
+  auto numberTransitions() const { return this->CV(); }
+
+  /**
+   *  @brief Return the ENDF designator for a specific shell
+   *
+   *  @param[in] index    the electron subshell index (one-based)
+   */
+  unsigned int designator( std::size_t index ) const {
+
+    return this->ivalue( 1, index );
+  }
+
+  /**
+   *  @brief Return the electron population for a specific shell
+   *
+   *  @param[in] index    the electron subshell index (one-based)
+   */
+  unsigned int population( std::size_t index ) const {
+
+    return this->ivalue( 2, index );
+  }
+
+  /**
+   *  @brief Return the binding energy for a specific shell
+   *
+   *  @param[in] index    the electron subshell index (one-based)
+   */
+  unsigned int bindingEnergy( std::size_t index ) const {
+
+    return this->ivalue( 3, index );
+  }
+
+  /**
+   *  @brief Return the vacancy probability for a specific shell
+   *
+   *  @param[in] index    the electron subshell index (one-based)
+   */
+  unsigned int vacancyProbality( std::size_t index ) const {
+
+    return this->ivalue( 4, index );
+  }
+
+  /**
+   *  @brief Return the number of transitions to fill a vacancy for a specific shell
+   *
+   *  @param[in] index    the electron subshell index (one-based)
+   */
+  unsigned int numberTransitions( std::size_t index ) const {
+
+    return this->ivalue( 5, index );
+  }
+
+  using ArrayData::empty;
+  using ArrayData::name;
+  using ArrayData::length;
+  using ArrayData::XSS;
+  using ArrayData::begin;
+  using ArrayData::end;
+};
+
+using SUBSH = PhotoatomicElectronSubshellBlock;
+
+} // block namespace
+} // ACEtk namespace
+} // njoy namespace
+
+#endif
---
+++ src/ACEtk/block/PhotoatomicElectronSubshellBlock/src/ctor.hpp
@@ -0,0 +1,41 @@
+PhotoatomicElectronSubshellBlock() = default;
+
+PhotoatomicElectronSubshellBlock( const PhotoatomicElectronSubshellBlock& ) = default;
+PhotoatomicElectronSubshellBlock( PhotoatomicElectronSubshellBlock&& ) = default;
+
+/**
+ *  @brief Constructor
+ *
+ *  @param[in] electrons        the number of electrons for each shell
+ *  @param[in] energies         the binding energy for each shell
+ *  @param[in] probabilities    the interaction probability for each shell
+ */
+PhotoatomicElectronSubshellBlock( std::vector< double > designators,
+                                  std::vector< unsigned int > electrons,
+                                  std::vector< double > energies,
+                                  std::vector< double > probabilities,
+                                  std::vector< unsigned int > transitions ) :
+  ArrayData( "SUBSH",
+             std::vector< std::vector< double > > {
+
+               std::move( designators ),
+               [] ( auto&& vector ) -> std::vector< double >
+                  { return { vector.begin(), vector.end() }; }( electrons ),
+               std::move( energies ),
+               std::move( probabilities ),
+               [] ( auto&& vector ) -> std::vector< double >
+                  { return { vector.begin(), vector.end() }; }( transitions )
+             } ) {}
+
+/**
+ *  @brief Constructor
+ *
+ *  @param[in] begin   the begin iterator of the EPS block in the XSS array
+ *  @param[in] end     the end iterator of the EPS block in the XSS array
+ *  @param[in] nsh     the number of electron shells
+ */
+PhotoatomicElectronSubshellBlock( Iterator begin, Iterator end, unsigned int nssh ) :
+  ArrayData( "SUBSH", begin, end, nssh, 5 ) {}
+
+PhotoatomicElectronSubshellBlock& operator=( const PhotoatomicElectronSubshellBlock& ) = default;
+PhotoatomicElectronSubshellBlock& operator=( PhotoatomicElectronSubshellBlock&& ) = default;
