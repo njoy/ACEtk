d46e7ea9fc995f19cdc146a0cd0420d6ec172ff4
Wim Haeck <whaeck@gmail.com>
2023-09-18 10:35:12 -0600

    Extending the xprob tests

--- python/test/block/Test_ACEtk_PhotoatomicSubshellTransitionDataBlock.py
+++ python/test/block/Test_ACEtk_PhotoatomicSubshellTransitionDataBlock.py
@@ -37,15 +37,101 @@ class Test_ACEtk_PhotoatomicSubshellTransitionDataBlock( unittest.TestCase ) :
 
             xs = chunk.transition_data(1)
 
+            self.assertEqual( 3, xs.NT )
+            self.assertEqual( 3, xs.number_transitions )
+
+            self.assertEqual( 4, len( xs.transition(1) ) )
+            self.assertEqual( 4, len( xs.transition(2) ) )
+            self.assertEqual( 4, len( xs.transition(3) ) )
+
+            self.assertAlmostEqual(    1, xs.transition(1)[0] )
+            self.assertAlmostEqual(    0, xs.transition(1)[1] )
+            self.assertAlmostEqual(  3.5, xs.transition(1)[2] )
+            self.assertAlmostEqual( 0.25, xs.transition(1)[3] )
+            self.assertAlmostEqual(    2, xs.transition(2)[0] )
+            self.assertAlmostEqual(    3, xs.transition(2)[1] )
+            self.assertAlmostEqual(  2.5, xs.transition(2)[2] )
+            self.assertAlmostEqual( 0.75, xs.transition(2)[3] )
+            self.assertAlmostEqual(    3, xs.transition(3)[0] )
+            self.assertAlmostEqual(    1, xs.transition(3)[1] )
+            self.assertAlmostEqual(  5.5, xs.transition(3)[2] )
+            self.assertAlmostEqual( 1.00, xs.transition(3)[3] )
+
+            self.assertEqual( 1, xs.primary_designator(1) )
+            self.assertEqual( 0, xs.secondary_designator(1) )
+            self.assertAlmostEqual( 3.5, xs.energy(1) )
+            self.assertAlmostEqual( 0.25, xs.probability(1) )
+            self.assertEqual( 2, xs.primary_designator(2) )
+            self.assertEqual( 3, xs.secondary_designator(2) )
+            self.assertAlmostEqual( 2.5, xs.energy(2) )
+            self.assertAlmostEqual( 0.75, xs.probability(2) )
+            self.assertEqual( 3, xs.primary_designator(3) )
+            self.assertEqual( 1, xs.secondary_designator(3) )
+            self.assertAlmostEqual( 5.5, xs.energy(3) )
+            self.assertAlmostEqual( 1.00, xs.probability(3) )
+
+            self.assertEqual( True, xs.is_radiative_transition(1) )
+            self.assertEqual( False, xs.is_radiative_transition(2) )
+            self.assertEqual( False, xs.is_radiative_transition(3) )
+
             xs = chunk.transition_data(2)
+
+            self.assertEqual( 0, xs.NT )
+            self.assertEqual( 0, xs.number_transitions )
 
             xs = chunk.transition_data(3)
 
+            self.assertEqual( 0, xs.NT )
+            self.assertEqual( 0, xs.number_transitions )
+
             xs = chunk.data[0]
+
+            self.assertEqual( 3, xs.NT )
+            self.assertEqual( 3, xs.number_transitions )
+
+            self.assertEqual( 4, len( xs.transition(1) ) )
+            self.assertEqual( 4, len( xs.transition(2) ) )
+            self.assertEqual( 4, len( xs.transition(3) ) )
+
+            self.assertAlmostEqual(    1, xs.transition(1)[0] )
+            self.assertAlmostEqual(    0, xs.transition(1)[1] )
+            self.assertAlmostEqual(  3.5, xs.transition(1)[2] )
+            self.assertAlmostEqual( 0.25, xs.transition(1)[3] )
+            self.assertAlmostEqual(    2, xs.transition(2)[0] )
+            self.assertAlmostEqual(    3, xs.transition(2)[1] )
+            self.assertAlmostEqual(  2.5, xs.transition(2)[2] )
+            self.assertAlmostEqual( 0.75, xs.transition(2)[3] )
+            self.assertAlmostEqual(    3, xs.transition(3)[0] )
+            self.assertAlmostEqual(    1, xs.transition(3)[1] )
+            self.assertAlmostEqual(  5.5, xs.transition(3)[2] )
+            self.assertAlmostEqual( 1.00, xs.transition(3)[3] )
+
+            self.assertEqual( 1, xs.primary_designator(1) )
+            self.assertEqual( 0, xs.secondary_designator(1) )
+            self.assertAlmostEqual( 3.5, xs.energy(1) )
+            self.assertAlmostEqual( 0.25, xs.probability(1) )
+            self.assertEqual( 2, xs.primary_designator(2) )
+            self.assertEqual( 3, xs.secondary_designator(2) )
+            self.assertAlmostEqual( 2.5, xs.energy(2) )
+            self.assertAlmostEqual( 0.75, xs.probability(2) )
+            self.assertEqual( 3, xs.primary_designator(3) )
+            self.assertEqual( 1, xs.secondary_designator(3) )
+            self.assertAlmostEqual( 5.5, xs.energy(3) )
+            self.assertAlmostEqual( 1.00, xs.probability(3) )
+
+            self.assertEqual( True, xs.is_radiative_transition(1) )
+            self.assertEqual( False, xs.is_radiative_transition(2) )
+            self.assertEqual( False, xs.is_radiative_transition(3) )
 
             xs = chunk.data[1]
 
+            self.assertEqual( 0, xs.NT )
+            self.assertEqual( 0, xs.number_transitions )
+
             xs = chunk.data[2]
+
+            self.assertEqual( 0, xs.NT )
+            self.assertEqual( 0, xs.number_transitions )
 
             # verify the xss array
             xss = chunk.xss_array
--- src/ACEtk/block/PhotoatomicSubshellTransitionDataBlock/test/PhotoatomicSubshellTransitionDataBlock.test.cpp
+++ src/ACEtk/block/PhotoatomicSubshellTransitionDataBlock/test/PhotoatomicSubshellTransitionDataBlock.test.cpp
@@ -97,13 +97,99 @@ void verifyChunk( const PhotoatomicSubshellTransitionDataBlock& chunk ) {
 
   auto xs = chunk.transitionData(1);
 
+  CHECK( 3 == xs.NT() );
+  CHECK( 3 == xs.numberTransitions() );
+
+  CHECK( 4 == xs.transition(1).size() );
+  CHECK( 4 == xs.transition(2).size() );
+  CHECK( 4 == xs.transition(3).size() );
+
+  CHECK(    1 == Approx( xs.transition(1)[0] ) );
+  CHECK(    0 == Approx( xs.transition(1)[1] ) );
+  CHECK(  3.5 == Approx( xs.transition(1)[2] ) );
+  CHECK( 0.25 == Approx( xs.transition(1)[3] ) );
+  CHECK(    2 == Approx( xs.transition(2)[0] ) );
+  CHECK(    3 == Approx( xs.transition(2)[1] ) );
+  CHECK(  2.5 == Approx( xs.transition(2)[2] ) );
+  CHECK( 0.75 == Approx( xs.transition(2)[3] ) );
+  CHECK(    3 == Approx( xs.transition(3)[0] ) );
+  CHECK(    1 == Approx( xs.transition(3)[1] ) );
+  CHECK(  5.5 == Approx( xs.transition(3)[2] ) );
+  CHECK( 1.00 == Approx( xs.transition(3)[3] ) );
+
+  CHECK(    1 == xs.primaryDesignator(1) );
+  CHECK(    0 == xs.secondaryDesignator(1) );
+  CHECK(  3.5 == Approx( xs.energy(1) ) );
+  CHECK( 0.25 == Approx( xs.probability(1) ) );
+  CHECK(    2 == xs.primaryDesignator(2) );
+  CHECK(    3 == xs.secondaryDesignator(2) );
+  CHECK(  2.5 == Approx( xs.energy(2) ) );
+  CHECK( 0.75 == Approx( xs.probability(2) ) );
+  CHECK(    3 == xs.primaryDesignator(3) );
+  CHECK(    1 == xs.secondaryDesignator(3) );
+  CHECK(  5.5 == Approx( xs.energy(3) ) );
+  CHECK( 1.00 == Approx( xs.probability(3) ) );
+
+  CHECK( true == xs.isRadiativeTransition(1) );
+  CHECK( false == xs.isRadiativeTransition(2) );
+  CHECK( false == xs.isRadiativeTransition(3) );
+
   xs = chunk.transitionData(2);
+
+  CHECK( 0 == xs.NT() );
+  CHECK( 0 == xs.numberTransitions() );
 
   xs = chunk.transitionData(3);
 
+  CHECK( 0 == xs.NT() );
+  CHECK( 0 == xs.numberTransitions() );
+
   xs = chunk.data()[0];
+
+  CHECK( 3 == xs.NT() );
+  CHECK( 3 == xs.numberTransitions() );
+
+  CHECK( 4 == xs.transition(1).size() );
+  CHECK( 4 == xs.transition(2).size() );
+  CHECK( 4 == xs.transition(3).size() );
+
+  CHECK(    1 == Approx( xs.transition(1)[0] ) );
+  CHECK(    0 == Approx( xs.transition(1)[1] ) );
+  CHECK(  3.5 == Approx( xs.transition(1)[2] ) );
+  CHECK( 0.25 == Approx( xs.transition(1)[3] ) );
+  CHECK(    2 == Approx( xs.transition(2)[0] ) );
+  CHECK(    3 == Approx( xs.transition(2)[1] ) );
+  CHECK(  2.5 == Approx( xs.transition(2)[2] ) );
+  CHECK( 0.75 == Approx( xs.transition(2)[3] ) );
+  CHECK(    3 == Approx( xs.transition(3)[0] ) );
+  CHECK(    1 == Approx( xs.transition(3)[1] ) );
+  CHECK(  5.5 == Approx( xs.transition(3)[2] ) );
+  CHECK( 1.00 == Approx( xs.transition(3)[3] ) );
+
+  CHECK(    1 == xs.primaryDesignator(1) );
+  CHECK(    0 == xs.secondaryDesignator(1) );
+  CHECK(  3.5 == Approx( xs.energy(1) ) );
+  CHECK( 0.25 == Approx( xs.probability(1) ) );
+  CHECK(    2 == xs.primaryDesignator(2) );
+  CHECK(    3 == xs.secondaryDesignator(2) );
+  CHECK(  2.5 == Approx( xs.energy(2) ) );
+  CHECK( 0.75 == Approx( xs.probability(2) ) );
+  CHECK(    3 == xs.primaryDesignator(3) );
+  CHECK(    1 == xs.secondaryDesignator(3) );
+  CHECK(  5.5 == Approx( xs.energy(3) ) );
+  CHECK( 1.00 == Approx( xs.probability(3) ) );
+
+  CHECK( true == xs.isRadiativeTransition(1) );
+  CHECK( false == xs.isRadiativeTransition(2) );
+  CHECK( false == xs.isRadiativeTransition(3) );
 
   xs = chunk.data()[1];
 
+  CHECK( 0 == xs.NT() );
+  CHECK( 0 == xs.numberTransitions() );
+
   xs = chunk.data()[2];
+
+  CHECK( 0 == xs.NT() );
+  CHECK( 0 == xs.numberTransitions() );
 }
