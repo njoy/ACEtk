039d6cb6e380755eaefe3e2594b9b84df92cadb9
Wim Haeck <whaeck@gmail.com>
2023-09-20 13:56:36 -0600

    Adding the BREME block on the C++ side

---
+++ src/ACEtk/block/ElectronBremsstrahlungEnergyDistributionBlock.hpp
@@ -0,0 +1,125 @@
+#ifndef NJOY_ACETK_BLOCK_ELECTRONBREMSTRAHLUNGENERGYDISTRIBUTIONBLOCK
+#define NJOY_ACETK_BLOCK_ELECTRONBREMSTRAHLUNGENERGYDISTRIBUTIONBLOCK
+
+// system includes
+
+// other includes
+#include "ACEtk/block/details/BaseElectronBlockWithLocators.hpp"
+#include "ACEtk/block/ElectronTabulatedEnergyDistribution.hpp"
+
+namespace njoy {
+namespace ACEtk {
+namespace block {
+
+/**
+ *  @class
+ *  @brief The photoatomic and electron BREMI and BREME block with the
+ *         photon energy distribution data from Bremsstrahlung
+ *
+ *  The ElectronElasticAngularDistributionBlock class a list of electron energies
+ *  for which angular distribution data is given.
+ */
+class ElectronBremsstrahlungEnergyDistributionBlock :
+    protected details::BaseElectronBlockWithLocators<
+                  ElectronBremsstrahlungEnergyDistributionBlock,
+                  ElectronTabulatedEnergyDistribution > {
+
+  friend class details::BaseElectronBlockWithLocators<
+                   ElectronBremsstrahlungEnergyDistributionBlock,
+                   ElectronTabulatedEnergyDistribution >;
+
+  /* fields */
+
+  /* auxiliary functions */
+
+public:
+
+  /* constructor */
+  #include "ACEtk/block/ElectronBremsstrahlungEnergyDistributionBlock/src/ctor.hpp"
+
+  /* methods */
+
+  /**
+   *  @brief Return the number of available energies
+   */
+  unsigned int NB() const { return BaseElectronBlockWithLocators::N(); }
+
+  /**
+   *  @brief Return the number of available energies
+   */
+  unsigned int numberEnergyPoints() const {
+
+    return BaseElectronBlockWithLocators::numberDataBlocks();
+  }
+
+  /**
+   *  @brief Return the relative distribution locator for an energy index
+   *
+   *  When the index is out of range an std::out_of_range exception is thrown
+   *  (debug mode only).
+   *
+   *  @param[in] index     the index (one-based)
+   */
+  std::size_t LLOC( std::size_t index ) const {
+
+    return BaseElectronBlockWithLocators::LLOC( index );
+  }
+
+  /**
+   *  @brief Return the relative distribution locator for an energy index
+   *
+   *  When the index is out of range an std::out_of_range exception is thrown
+   *  (debug mode only).
+   *
+   *  @param[in] index     the index (one-based)
+   */
+  std::size_t distributionLocator( std::size_t index ) const {
+
+    return BaseElectronBlockWithLocators::locator( index );
+  }
+
+  /**
+   *  @brief Return the energy values
+   */
+  auto energies() const {
+
+    return BaseElectronBlockWithLocators::energies();
+  }
+
+  /**
+   *  @brief Return all energy distribution data
+   */
+  const std::vector< ElectronTabulatedEnergyDistribution >& distributions() const {
+
+    return BaseElectronBlockWithLocators::data();
+  }
+
+  /**
+   *  @brief Return the energy distribution for an energy index
+   *
+   *  When the index is out of range an std::out_of_range exception is thrown
+   *  (debug mode only).
+   *
+   *  @param[in] index     the index (one-based)
+   */
+  const ElectronTabulatedEnergyDistribution&
+  distribution( std::size_t index ) const {
+
+    return BaseElectronBlockWithLocators::data( index );
+  }
+
+  using BaseElectronBlockWithLocators::empty;
+  using BaseElectronBlockWithLocators::name;
+  using BaseElectronBlockWithLocators::length;
+  using BaseElectronBlockWithLocators::XSS;
+  using BaseElectronBlockWithLocators::begin;
+  using BaseElectronBlockWithLocators::end;
+};
+
+using BREME = ElectronBremsstrahlungEnergyDistributionBlock;
+
+} // block namespace
+} // ACEtk namespace
+} // njoy namespace
+
+#endif
---
+++ src/ACEtk/block/ElectronBremsstrahlungEnergyDistributionBlock/test/ElectronBremsstrahlungEnergyDistributionBlock.test.cpp
@@ -0,0 +1,119 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "ACEtk/block/ElectronBremsstrahlungEnergyDistributionBlock.hpp"
+
+// other includes
+
+// convenience typedefs
+using namespace njoy::ACEtk;
+using ElectronBremsstrahlungEnergyDistributionBlock = block::ElectronBremsstrahlungEnergyDistributionBlock;
+using ElectronTabulatedEnergyDistribution = block::ElectronTabulatedEnergyDistribution;
+
+std::vector< double > chunk();
+void verifyChunk( const ElectronBremsstrahlungEnergyDistributionBlock& );
+
+SCENARIO( "ElectronBremsstrahlungEnergyDistributionBlock" ) {
+
+  GIVEN( "valid data for a ElectronBremsstrahlungEnergyDistributionBlock instance" ) {
+
+    std::vector< double > xss = chunk();
+
+    WHEN( "the data is given explicitly" ) {
+
+      std::vector< ElectronTabulatedEnergyDistribution > distributions = {
+
+        ElectronTabulatedEnergyDistribution( 1e-11, { 1e-12, 9e-12 }, { 0., 1. } ),
+        ElectronTabulatedEnergyDistribution(  1e-6, { 1e-11, 1e-9, 9e-7 }, { 0., 0.75, 1. } ),
+        ElectronTabulatedEnergyDistribution(  1e-3, { 1e-11, 1e-6, 1e-4, 9e-4 }, { 0., 0.25, 0.5, 1. } ),
+        ElectronTabulatedEnergyDistribution(    1., { 1e-11, 0.999 }, { 0., 1. } )
+      };
+
+      ElectronBremsstrahlungEnergyDistributionBlock chunk( std::move( distributions ) );
+
+      THEN( "a ElectronBremsstrahlungEnergyDistributionBlock can be constructed and members can be tested" ) {
+
+        verifyChunk( chunk );
+      } // THEN
+
+      THEN( "the XSS array is correct" ) {
+
+        auto xss_chunk = chunk.XSS();
+        for ( unsigned int i = 0; i < chunk.length(); ++i ) {
+
+          CHECK( xss[i] == Approx( xss_chunk[i] ) );
+        }
+      } // THEN
+    } // WHEN
+
+    WHEN( "the data is defined by iterators" ) {
+
+      ElectronBremsstrahlungEnergyDistributionBlock chunk( xss.begin(), xss.end(), 4 );
+
+      THEN( "a ElectronBremsstrahlungEnergyDistributionBlock can be constructed and members can be tested" ) {
+
+        verifyChunk( chunk );
+      } // THEN
+
+      THEN( "the XSS array is correct" ) {
+
+        auto xss_chunk = chunk.XSS();
+        for ( unsigned int i = 0; i < chunk.length(); ++i ) {
+
+          CHECK( xss[i] == Approx( xss_chunk[i] ) );
+        }
+      } // THEN
+    } // WHEN
+  } // GIVEN
+} // SCENARIO
+
+std::vector< double > chunk() {
+
+  // first value of the second xs increased by 1 for testing purposes
+  return {
+
+    1e-11, 1e-6, 1e-3, 1.,
+        2,    3,    4,  2,
+        0,    4,   10, 18,
+    1e-12, 9e-12, 0., 1.,
+    1e-11, 1e-9, 9e-7, 0., 0.75, 1.,
+    1e-11, 1e-6, 1e-4, 9e-4, 0., 0.25, 0.5, 1.,
+    1e-11, 0.999, 0., 1. };
+}
+
+void verifyChunk( const ElectronBremsstrahlungEnergyDistributionBlock& chunk ) {
+
+  CHECK( false == chunk.empty() );
+  CHECK( 34 == chunk.length() );
+  CHECK( "BREME" == chunk.name() );
+
+  CHECK( 4 == chunk.NB() );
+  CHECK( 4 == chunk.numberEnergyPoints() );
+  CHECK( 4 == chunk.energies().size() );
+  CHECK( 4 == chunk.distributions().size() );
+
+  CHECK(  1 == chunk.LLOC(1) );
+  CHECK(  5 == chunk.LLOC(2) );
+  CHECK( 11 == chunk.LLOC(3) );
+  CHECK( 19 == chunk.LLOC(4) );
+  CHECK(  1 == chunk.distributionLocator(1) );
+  CHECK(  5 == chunk.distributionLocator(2) );
+  CHECK( 11 == chunk.distributionLocator(3) );
+  CHECK( 19 == chunk.distributionLocator(4) );
+
+  auto distribution = chunk.distribution(1);
+  CHECK( 1e-11 == distribution.energy() );
+  CHECK( 2 == distribution.numberOutgoingEnergies() );
+
+  distribution = chunk.distribution(2);
+  CHECK( 1e-6 == distribution.energy() );
+  CHECK( 3 == distribution.numberOutgoingEnergies() );
+
+  distribution = chunk.distribution(3);
+  CHECK( 1e-3 == distribution.energy() );
+  CHECK( 4 == distribution.numberOutgoingEnergies() );
+
+  distribution = chunk.distribution(4);
+  CHECK( 1. == distribution.energy() );
+  CHECK( 2 == distribution.numberOutgoingEnergies() );
+}
