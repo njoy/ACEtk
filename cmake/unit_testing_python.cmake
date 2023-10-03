3833beeb9a5d834627eea99f3b28a3d05eaafa4a
Wim Haeck <whaeck@gmail.com>
2023-10-03 11:17:08 -0600

    Updating list of python unit tests

--- cmake/unit_testing_python.cmake
+++ cmake/unit_testing_python.cmake
@@ -34,7 +34,6 @@ add_python_test( Table      Test_ACEtk_Table.py )
 # add_python_test( continuous.EnergyDistributionBlock                      continuous/Test_ACEtk_continuous_EnergyDistributionBlock.py )
 # add_python_test( continuous.FissionMultiplicityBlock                     continuous/Test_ACEtk_continuous_FissionMultiplicityBlock.py )
 # add_python_test( continuous.FullyIsotropicDistribution                   continuous/Test_ACEtk_continuous_FullyIsotropicDistribution.py )
-# add_python_test( continuous.InterpolationData                            continuous/Test_ACEtk_continuous_InterpolationData.py )
 
 add_python_test( continuous.PolynomialFissionMultiplicity                continuous/Test_ACEtk_continuous_PolynomialFissionMultiplicity.py )
 add_python_test( continuous.TwoBodyTransferDistribution                  continuous/Test_ACEtk_continuous_TwoBodyTransferDistribution.py )
