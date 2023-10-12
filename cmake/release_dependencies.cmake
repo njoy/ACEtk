c3920b1da75a8bc8c17fa65f44c47153f5c35e9f
Wim Haeck <whaeck@gmail.com>
2023-10-11 23:01:10 -0600

    Updating release dependencies

--- cmake/release_dependencies.cmake
+++ cmake/release_dependencies.cmake
@@ -5,11 +5,6 @@ include( FetchContent )
 # Declare project dependencies
 #######################################################################
 
-FetchContent_Declare( tools
-    GIT_REPOSITORY  https://github.com/njoy/tools
-    GIT_TAG         25c9273d05601a9644feea6d7539250bf1d1c0dc # tag: v0.2.0
-    )
-
 FetchContent_Declare( Catch2
     GIT_REPOSITORY  https://github.com/catchorg/Catch2
     GIT_TAG         3f0283de7a9c43200033da996ff9093be3ac84dc # tag: v3.3.2
@@ -34,15 +29,19 @@ FetchContent_Declare( spdlog
     GIT_REPOSITORY  https://github.com/gabime/spdlog
     GIT_TAG         ad0e89cbfb4d0c1ce4d097e134eb7be67baebb36 # tag: v1.11.0
     )
-set( SPDLOG_BUILD_PIC CACHE INTERNAL BOOL ON )
+
+FetchContent_Declare( tools
+    GIT_REPOSITORY  https://github.com/njoy/tools
+    GIT_TAG         25c9273d05601a9644feea6d7539250bf1d1c0dc # tag: v0.2.0
+    )
 
 #######################################################################
 # Load dependencies
 #######################################################################
 
 FetchContent_MakeAvailable(
-    tools
     disco
     range-v3
     spdlog
+    tools
     )
