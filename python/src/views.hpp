063e555f396068c30e233e6d1866e9671eee7ec9
Wim Haeck <whaeck@gmail.com>
2024-03-07 20:46:16 -0700

    Removing range-v3 from views.hpp in python bindings

--- python/src/views.hpp
+++ python/src/views.hpp
@@ -7,25 +7,24 @@
 // other includes
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "range/v3/view/any_view.hpp"
-#include "range/v3/range/conversion.hpp"
-#include "range/v3/range/operations.hpp"
+#include "tools/views/AnyView.hpp"
 
 namespace python = pybind11;
+namespace std20 = nano;
 
 /**
  *  @brief Type aliases for basic any views
  *
- *  The Element template represents the type of the any_view element.
+ *  The ReferenceType template represents the type of the AnyView reference type.
  */
-template < typename Element >
-using BasicInputAnyView = ranges::any_view< Element, ranges::category::input >;
-template < typename Element >
-using BasicForwardAnyView = ranges::any_view< Element, ranges::category::forward >;
-template < typename Element >
-using BasicBidirectionalAnyView = ranges::any_view< Element, ranges::category::bidirectional >;
-template < typename Element >
-using BasicRandomAccessAnyView = ranges::any_view< Element, ranges::category::random_access >;
+template < typename ReferenceType >
+using BasicInputAnyView = njoy::tools::views::AnyInputView< ReferenceType >;
+template < typename ReferenceType >
+using BasicForwardAnyView = njoy::tools::views::AnyForwardView< ReferenceType >;
+template < typename ReferenceType >
+using BasicBidirectionalAnyView = njoy::tools::views::AnyBidirectionalView< ReferenceType >;
+template < typename ReferenceType >
+using BasicRandomAccessAnyView = njoy::tools::views::AnyRandomAccessView< ReferenceType >;
 
 /**
  *  @brief Type aliases for any views using a reference wrapper to a const
@@ -36,16 +35,14 @@ using BasicRandomAccessAnyView = ranges::any_view< Element, ranges::category::ra
  *  containing reference wrappers (e.g. variants of reference wrappers) should
  *  continue to use the Basic... versions of the any views.
  */
-template < typename Element >
-using RefWrapper = std::reference_wrapper< const Element >;
-template < typename Element >
-using InputAnyView = BasicInputAnyView< RefWrapper< Element > >;
-template < typename Element >
-using ForwardAnyView = BasicForwardAnyView< RefWrapper< Element > >;
-template < typename Element >
-using BidirectionalAnyView = BasicBidirectionalAnyView< RefWrapper< Element > >;
-template < typename Element >
-using RandomAccessAnyView = BasicRandomAccessAnyView< RefWrapper< Element > >;
+template < typename ReferenceType >
+using InputAnyView = BasicInputAnyView< const ReferenceType& >;
+template < typename ReferenceType >
+using ForwardAnyView = BasicForwardAnyView< const ReferenceType& >;
+template < typename ReferenceType >
+using BidirectionalAnyView = BasicBidirectionalAnyView< const ReferenceType& >;
+template < typename ReferenceType >
+using RandomAccessAnyView = BasicRandomAccessAnyView< const ReferenceType& >;
 
 /**
  *  @brief Some recurring basic views
@@ -70,32 +67,32 @@ void wrapBasicBidirectionalAnyViewOf( python::module& module, const std::string&
   python::class_< BasicBidirectionalAnyView< Element > >( module, name.c_str() )
   .def( "__len__",
         [] ( BasicBidirectionalAnyView< Element >& view )
-           { return ranges::distance( view ); },
+           { return std::distance( view.begin(), view.end() ); },
         "Return the length of the sequence" )
   .def( "__iter__",
         [] ( BasicBidirectionalAnyView< Element >& view )
            { return python::make_iterator<
                         python::return_value_policy::reference_internal,
-                        ranges::iterator_t< BasicBidirectionalAnyView< Element > >,
-                        ranges::sentinel_t< BasicBidirectionalAnyView< Element > >,
+                        nano::iterator_t< BasicBidirectionalAnyView< Element > >,
+                        nano::sentinel_t< BasicBidirectionalAnyView< Element > >,
                         Element >( view.begin(), view.end() ); },
         "Return an iterator for the sequence",
         python::keep_alive< 0, 1 >() )
   .def( "front",
         [] ( BasicBidirectionalAnyView< Element >& view )
-           { return ranges::front( view ); },
+           { return view.front(); },
         "Return the front element of the sequence",
         python::keep_alive< 0, 1 >() )
   .def( "to_list",
         [] ( BasicBidirectionalAnyView< Element >& view )
            -> std::vector< Element >
-           { return ranges::to< std::vector< Element > >( view ); },
+           { return std::vector< Element >( view.begin(), view.end() ); },
         "Convert the sequence to a list (this is a deep copy for primitive\n"
         "types (like integer and floats) and a shallow copy for custom types" )
   .def( "copy",
       	[] ( BasicRandomAccessAnyView< Element >& view )
            -> std::vector< CopyElement >
-           { return ranges::to< std::vector< CopyElement > >( view ); },
+           { return std::vector< CopyElement >( view.begin(), view.end() ); },
         "Copy the sequence to a list (this is a deep copy for both primitive\n"
         "types (like integer and floats) and custom types" );
 }
@@ -125,12 +122,12 @@ void wrapBasicRandomAccessAnyViewOf( python::module& module, const std::string& 
   python::class_< BasicRandomAccessAnyView< Element > >( module, name.c_str() )
   .def( "__len__",
         [] ( BasicRandomAccessAnyView< Element >& view )
-           { return ranges::distance( view ); },
+           { return std::distance( view.begin(), view.end() ); },
         "Return the length of the sequence" )
   .def( "__getitem__",
         [index] ( BasicRandomAccessAnyView< Element >& view, int i ) {
 
-          return ranges::index( view, index( i, ranges::distance( view ) ) );
+          return view[index( i, std::distance( view.begin(), view.end() ) )];
         },
         "Return the value in the sequence at the given index or throw an\n"
         "exception when the index is out of range",
@@ -140,7 +137,7 @@ void wrapBasicRandomAccessAnyViewOf( python::module& module, const std::string& 
              const python::slice& slice ) {
 
           python::size_t start, stop, step, length;
-          if ( !slice.compute( ranges::distance( view ),
+          if ( !slice.compute( std::distance( view.begin(), view.end() ),
                                &start, &stop, &step, &length ) ) {
 
             throw python::error_already_set();
@@ -148,7 +145,7 @@ void wrapBasicRandomAccessAnyViewOf( python::module& module, const std::string& 
           std::vector< Element > list;
           for ( unsigned int i = 0; i < length; ++i ) {
 
-             list.emplace_back( ranges::index( view, start ) );
+             list.emplace_back( view[start] );
              start += step;
           }
           return list;
@@ -162,26 +159,26 @@ void wrapBasicRandomAccessAnyViewOf( python::module& module, const std::string& 
         [] ( BasicRandomAccessAnyView< Element >& view )
            { return python::make_iterator<
                         python::return_value_policy::reference_internal,
-                        ranges::iterator_t< BasicRandomAccessAnyView< Element > >,
-                        ranges::sentinel_t< BasicRandomAccessAnyView< Element > >,
+                        nano::iterator_t< BasicBidirectionalAnyView< Element > >,
+                        nano::sentinel_t< BasicBidirectionalAnyView< Element > >,
                         Element >( view.begin(), view.end() ); },
         "Return an iterator for the sequence",
         python::keep_alive< 0, 1 >() )
   .def( "front",
         [] ( BasicRandomAccessAnyView< Element >& view )
-           { return ranges::front( view ); },
+           { return view.front(); },
         "Return the front element of the sequence",
         python::keep_alive< 0, 1 >() )
   .def( "to_list",
-      	[] ( BasicRandomAccessAnyView< Element >& view )
+        [] ( BasicRandomAccessAnyView< Element >& view )
            -> std::vector< Element >
-           { return ranges::to< std::vector< Element > >( view ); },
+           { return std::vector< Element >( view.begin(), view.end() ); },
         "Convert the sequence to a list (this is a deep copy for primitive\n"
         "types (like integer and floats) and a shallow copy for custom types" )
   .def( "copy",
       	[] ( BasicRandomAccessAnyView< Element >& view )
            -> std::vector< CopyElement >
-           { return ranges::to< std::vector< CopyElement > >( view ); },
+           { return std::vector< CopyElement >( view.begin(), view.end() ); },
         "Copy the sequence to a list (this is a deep copy for both primitive\n"
         "types (like integer and floats) and custom types" );
 }
@@ -195,7 +192,7 @@ void wrapBasicRandomAccessAnyViewOf( python::module& module, const std::string& 
 template < typename Element >
 void wrapBidirectionalAnyViewOf( python::module& module, const std::string& name ) {
 
-  wrapBasicBidirectionalAnyViewOf< RefWrapper< Element >, Element >( module, name );
+  wrapBasicBidirectionalAnyViewOf< const Element&, Element >( module, name );
 }
 
 /**
@@ -207,7 +204,7 @@ void wrapBidirectionalAnyViewOf( python::module& module, const std::string& name
 template < typename Element >
 void wrapRandomAccessAnyViewOf( python::module& module, const std::string& name ) {
 
-  wrapBasicRandomAccessAnyViewOf< RefWrapper< Element >, Element >( module, name );
+  wrapBasicRandomAccessAnyViewOf< const Element&, Element >( module, name );
 }
 
 #endif
