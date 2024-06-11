d3dbf0d1059f169a3cf5f3aa79cc3125be29bd6e
Tim Burke <tpburke@lanl.gov>
2024-06-11 10:37:44 -0600

    Moves all shacl_FetchContent calls to dependencies.cmake

--- cmake/develop_dependencies.cmake
+++ cmake/develop_dependencies.cmake
@@ -12,21 +12,31 @@ shacl_FetchContent_Declare( tools
     GIT_SHALLOW     TRUE
     )
 
-shacl_FetchContent_Declare( Catch2
-    GIT_REPOSITORY  ../../catchorg/catch2
-    GIT_TAG         v3.3.2
-    GIT_SHALLOW     TRUE
-    )
-
-shacl_FetchContent_Declare( pybind11
-    GIT_REPOSITORY  ../../pybind/pybind11
-    GIT_TAG         v2.10.4
-    GIT_SHALLOW     TRUE
-    )
-
 #######################################################################
 # Load dependencies
 #######################################################################
+
+if(ACEtk.python)
+  shacl_FetchContent_Declare( pybind11
+      GIT_REPOSITORY  ../../pybind/pybind11
+      GIT_TAG         v2.10.4
+      GIT_SHALLOW     TRUE
+      )
+  shacl_FetchContent_MakeAvailable(
+    pybind11
+    )
+endif()
+
+if(ACEtk.tests)
+  shacl_FetchContent_Declare( Catch2
+      GIT_REPOSITORY  ../../catchorg/catch2
+      GIT_TAG         v3.3.2
+      GIT_SHALLOW     TRUE
+      )
+  shacl_FetchContent_MakeAvailable(
+      Catch2 
+    )
+endif()
 
 shacl_FetchContent_MakeAvailable(
     tools
--- cmake/release_dependencies.cmake
+++ cmake/release_dependencies.cmake
@@ -6,19 +6,9 @@ include( shacl_FetchContent )
 # Declare project dependencies
 #######################################################################
 
-shacl_FetchContent_Declare( Catch2
-    GIT_REPOSITORY  ../../catchorg/Catch2
-    GIT_TAG         3f0283de7a9c43200033da996ff9093be3ac84dc # tag: v3.3.2
-    )
-
 shacl_FetchContent_Declare( FastFloat
     GIT_REPOSITORY  ../../fastfloat/fast_float
     GIT_TAG         f476bc713fda06fbd34dc621b466745a574b3d4c # tag: v6.1.1
-    )
-
-shacl_FetchContent_Declare( pybind11
-    GIT_REPOSITORY  ../../pybind/pybind11
-    GIT_TAG         5b0a6fc2017fcc176545afe3e09c9f9885283242 # tag: v2.10.4
     )
 
 shacl_FetchContent_Declare( spdlog
@@ -36,8 +26,29 @@ shacl_FetchContent_Declare( tools
 # Load dependencies
 #######################################################################
 
+if(ACEtk.python)
+  shacl_FetchContent_Declare( pybind11
+      GIT_REPOSITORY  ../../pybind/pybind11
+      GIT_TAG         5b0a6fc2017fcc176545afe3e09c9f9885283242 # tag: v2.10.4
+      )
+  shacl_FetchContent_MakeAvailable(
+    pybind11
+    )
+endif()
+
+if(ACEtk.tests)
+  shacl_FetchContent_Declare( Catch2
+      GIT_REPOSITORY  ../../catchorg/Catch2
+      GIT_TAG         3f0283de7a9c43200033da996ff9093be3ac84dc # tag: v3.3.2
+      )
+  shacl_FetchContent_MakeAvailable(
+      Catch2 
+    )
+endif()
+
 shacl_FetchContent_MakeAvailable(
     FastFloat
     spdlog
     tools
     )
+
