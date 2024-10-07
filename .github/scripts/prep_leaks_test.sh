1b8e8eac34999f3bc16a42d489bfba336e15a4b0
Joe Peterson <joewpeterson@lanl.gov>
2024-10-07 17:13:29 -0600

    Add memory leak checks for all tests

---
+++ .github/scripts/prep_leaks_test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "
+check_return_code() {
+        if [ $? -ne 0 ]; then
+                echo \"Memory leak detected. Test Failed...\"
+                exit 1
+        fi
+} 
+
+" > test_leaks.sh
+
+find . -iname "*.test" -type f | while IFS= read -r line; do
+	dirname=$(dirname "$line")
+	realname=$(basename "$line")
+	echo "cd ${dirname}; leaks --atExit -- ./${realname}; check_return_code; cd -" >> test_leaks.sh
+done
