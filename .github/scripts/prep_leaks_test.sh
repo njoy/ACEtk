3544ea8183abf18c5c624039bd4ceba60fd71f0b
Joe Peterson <joewpeterson@lanl.gov>
2024-10-08 15:38:16 -0600

    turn off debug information in valgrind and remove mac leaks check

--- .github/scripts/prep_leaks_test.sh
+++ .github/scripts/prep_leaks_test.sh
@@ -9,11 +9,11 @@ check_return_code() {
 
 " > test_leaks.sh
 
-#find . -iname "*.test" -type f | while IFS= read -r line; do
-#	dirname=$(dirname "$line")
-#	realname=$(basename "$line")
-#	echo "cd ${dirname}; leaks --atExit -- ./${realname}; check_return_code; cd -" >> test_leaks.sh
-#done
+find . -iname "*.test" -type f | while IFS= read -r line; do
+	dirname=$(dirname "$line")
+	realname=$(basename "$line")
+	echo "cd ${dirname}; leaks --nostacks --atExit -- ./${realname}; check_return_code; cd -" >> test_leaks.sh
+done
 
 echo "exit 0" >> test_leaks.sh
 
