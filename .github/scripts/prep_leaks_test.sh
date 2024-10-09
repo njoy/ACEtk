705911b5bc7e6474e0b80439ecc867fe4e7b8c92
Joe Peterson <joewpeterson@lanl.gov>
2024-10-09 10:43:44 -0600

    Test re-inabled mac

--- .github/scripts/prep_leaks_test.sh
+++ .github/scripts/prep_leaks_test.sh
@@ -12,7 +12,7 @@ check_return_code() {
 find . -iname "*.test" -type f | while IFS= read -r line; do
 	dirname=$(dirname "$line")
 	realname=$(basename "$line")
-	echo "cd ${dirname}; leaks --nostacks --atExit -- ./${realname}; check_return_code; cd -" >> test_leaks.sh
+	echo "cd ${dirname}; leaks --noContent --nostacks --atExit -- ./${realname}; check_return_code; cd -" >> test_leaks.sh
 done
 
 echo "exit 0" >> test_leaks.sh
