29f970f0322ac1950231b608b4d2bb1224494c8b
Amanda Lewis <amlewis1578@gmail.com>
2025-04-10 13:51:25 -0600

    updating tools version to include windows fixes

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,7 +8,7 @@ include( shacl_FetchContent )
 
 shacl_FetchContent_Declare( tools
     GIT_REPOSITORY  ../../njoy/tools
-    GIT_TAG         91c5fe6f3ccea5abc17d42c1efbdce4b1b2f0859  # CHANGE THIS: NOT A FINAL RELEASE
+    GIT_TAG         af4f1cb3a7072bbc2f45c6d43e76ddd04f643d4e  # CHANGE THIS: NOT A FINAL RELEASE
     )
 
 #######################################################################
