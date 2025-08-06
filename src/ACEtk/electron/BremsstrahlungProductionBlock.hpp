e66c603693517cd98af70763621f62a58828bede
Michael A. Lively <livelym@lanl.gov>
2025-08-06 11:53:54 -0600

    Add EL03 bremsstrahlung production block implementation.

--- src/ACEtk/electron/BremsstrahlungProductionBlock.hpp
+++ src/ACEtk/electron/BremsstrahlungProductionBlock.hpp
@@ -4,7 +4,7 @@
 // system includes
 
 // other includes
-#include "ACEtk/base/ArrayData.hpp"
+#include "ACEtk/base/MatrixData.hpp"
 
 namespace njoy {
 namespace ACEtk {
@@ -28,7 +28,7 @@ namespace electron {
  *  stored in NXS(6). The size of the bremsstrahlung production cross
  *  section array (the number of values for interpolation) is NEB*NPB.
  */
-class BremsstrahlungProductionBlock : protected base::ArrayData {
+class BremsstrahlungProductionBlock : protected base::MatrixData {
 
   /* fields */
   
@@ -37,10 +37,97 @@ class BremsstrahlungProductionBlock : protected base::ArrayData {
 public:
 
   /* constructor */
+  #include "ACEtk/electron/BremsstrahlungProductionBlock/src/ctor.hpp"
   
   /* methods */
   
-
+  /**
+   *  @brief Return the number of electron energy points
+   */
+  unsigned int NEB() const { return this->M(); }
+  
+  /**
+   *  @brief Return the number of electron energy points
+   */
+  unsigned int numberElectronEnergies() const { return this->NEB(); }
+  
+  /**
+   *  @brief Return the number of photon energy ratio points
+   */
+  unsigned int NPB() const { return this->N(); }
+  
+  /**
+   *  @brief Return the number of photon energy ratio points
+   */
+  unsigned int numberPhotonRatios() const { return this->NPB(); }
+  
+  /**
+   *  @brief Return the number of cross section values
+   */
+  unsigned int numberCrossSections() const { return this->numberValues(); }
+  
+  /**
+   *  @brief Return a single electron energy point
+   *
+   *  @param[in] index    the electron energy index (one-based)
+   */
+  double electronEnergy( std::size_t index ) const {
+    
+    return this->drowLabel( index );
+  }
+  
+  /**
+   *  @brief Return the electron energy points
+   */
+  auto electronEnergies() const { return this->drowLabels(); }
+  
+  /**
+   *  @brief Return a single photon energy ratio point
+   *
+   *  @param[in] index    the photon energy ratio index (one-based)
+   */
+  double photonRatio( std::size_t index ) const {
+    
+    return this->dcolumnLabel( index );
+  }
+  
+  /**
+   *  @brief Return the photon energy ratio points
+   */
+  auto photonRatios() const { return this->dcolumnLabels(); }
+  
+  /**
+   *  @brief Return a single bremsstrahlung cross section value
+   *
+   *  @param[in] eeIndex    the electron energy index (one-based)
+   *  @param[in] prIndex    the photon energy ratio index (one-based)
+   */
+  double crossSection( std::size_t eeIndex, std::size_t prIndex ) const {
+    
+    return this->dvalue( eeIndex, prIndex );
+  }
+  
+  /**
+   *  @brief Return the cross section values for a given electron energy index
+   *
+   *  @param[in] index    the electron energy index (one-based)
+   */
+  auto crossSections( std::size_t index ) const {
+    
+    return this->drow( index );
+  }
+  
+  /**
+   *  @brief Return the bremsstrahlung cross section data array
+   */
+  auto crossSectionData() const { return this->dmatrix(); }
+  
+  using MatrixData::empty;
+  using MatrixData::name;
+  using MatrixData::length;
+  using MatrixData::XSS;
+  using MatrixData::begin;
+  using MatrixData::end;
 };
 
 using XSB = BremsstrahlungProductionBlock;
